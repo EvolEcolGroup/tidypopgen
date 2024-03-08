fc9e7c5dcd0e48e952b4212b1244888d0a34f992
Andrea Manica <am315@cam.ac.uk>
2024-03-08 22:57:24 +0000

    clean non-interactive gt_find_clusters

--- data-raw/find.clusters.R
+++ data-raw/find.clusters.R
@@ -5,9 +5,6 @@
 
 gt_find_clusters <- function(x, gt_pca=NULL, clust = NULL, n.pca = NULL, n.clust = NULL,
                                    method = c("kmeans", "ward"),
-                                   stat = c("BIC", "AIC", "WSS"),
-                                   choose.n.clust = TRUE,
-                                   criterion = c("diffNgroup", "min","goesup", "smoothNgoesup", "goodfit"),
                                    max.n.clust = round(nrow(x)/10), n.iter = 1e5, n.start = 10,
                                    scale = FALSE){
 
@@ -16,28 +13,55 @@ gt_find_clusters <- function(x, gt_pca=NULL, clust = NULL, n.pca = NULL, n.clust
   }
 
   ## CHECKS ##
-  stat <- match.arg(stat)
-
+  method <- match.arg(method)
 
   ## SOME GENERAL VARIABLES ##
-  N <- nrow(x)
+  N <- nrow(gt_pca$scores)
   min.n.clust <- 2
+  max.n.clust <- max(max.n.clust, 2)
 
   if(is.null(n.pca)){
     n.pca <- tidy(gt_pca ,matrix="eigenvalues") %>% dplyr::filter(.data$cumulative<0.9) %>% nrow() +1
+    message("'n.pca' was not set: ", n.pca, " components were used")
   }
 
-  ## convert PCA
-  pcaX <- adegenet:::.glPca2dudi(gt_pca)
+  nbClust <- min.n.clust:max.n.clust
+  cluster_list <- list(k = seq_len(max.n.clust),
+                       WSS = numeric(0),
+                       AIC = numeric(0),
+                       BIC = numeric(0),
+                       groups = list())
+  WSS <- numeric(0)
+
+  for(i in 1:length(nbClust)){
+    if (method == "kmeans") {
+      ## kmeans clustering (original method)
+      groups_assignments <- kmeans(gt_pca$scores[,seq_len(n.pca)], centers = nbClust[i], iter.max = n.iter, nstart = n.start)$cluster
+    } else {
+      ## ward clustering
+      groups_assignments <- cutree(hclust(dist(gt_pca$scores[,seq_len(n.pca)])^2, method = "ward.D2"), k = nbClust[i])
+    }
+    WSS[i] <- .compute.wss(gt_pca$scores[,seq_len(n.pca)], groups_assignments)
+    cluster_list$groups[[i+1]]<- groups_assignments
+
+  }
+  # compute statistics of goodnees of fit
+  WSS.ori <- sum(apply(gt_pca$scores[,seq_len(n.pca)], 2, function(v) sum((v-mean(v))^2) ))
+  cluster_list$AIC <- N*log(c(WSS.ori,WSS)/N) + 2*c(1,nbClust)
+  cluster_list$BIC <- N*log(c(WSS.ori,WSS)/N) + log(N) *c(1,nbClust)
+  cluster_list$WSS <- c(WSS.ori, WSS)
+  names(cluster_list$groups)<-seq_len(1:max.n.clust)
+
+  class(cluster_list) <- c("gt_clusters", class(cluster_list))
+  return(cluster_list)
+
+}
 
 
-  ## CALL DATA.FRAME METHOD
-  res <- find.clusters(pcaX$li, clust=clust, n.pca=n.pca, n.clust=n.clust,
-                       stat=stat, max.n.clust=max.n.clust, n.iter=n.iter, n.start=n.start,
-                       choose.n.clust=choose.n.clust, method = method,
-                       criterion=criterion, center=FALSE, scale=FALSE, dudi=pcaX)
-  return(res)
-} # end find.clusters.genlight
 
 
-
+## Compute within sum of squares from a matrix 'x' and a factor 'f'
+.compute.wss <- function(x, f) {
+  x.group.mean <- apply(x, 2, tapply, f, mean)
+  sum((x - x.group.mean[as.character(f),])^2)
+}
