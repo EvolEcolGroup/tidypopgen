7e35f50e487a6f8c309453758be2398f135bf818
Andrea Manica <am315@cam.ac.uk>
2024-03-09 11:05:04 +0000

    dapc

--- data-raw/dapc.R
+++ data-raw/dapc.R
@@ -1,107 +1,67 @@
 #################
-## dapc.genlight
+## dapc for gen_tibble
 #################
-#' @method dapc gen_tibble
 #' @export
-dapc.gen_tibble <- function(x, pop=NULL, n.pca=NULL, n.da=NULL,
+gt_dapc <- function(x, pop=NULL, n.pca=NULL, n.da=NULL,
                           scale=FALSE,  var.contrib=TRUE, var.loadings=FALSE, pca.info=TRUE,
                           pca.select=c("nbEig","percVar"), perc.pca=NULL, glPca=NULL, ...){
   pca.select <- match.arg(pca.select)
 
   if(is.null(pop)) {
-    pop.fac <- x$population
+    if (any(!inherits(x,"gt_pca_clust"),is.null(x$n_clust))){
+      stop("if 'pop' is not set, 'x' should be a 'gt_pca_clust ")
+    }
+    pop.fac <- as.factor(x$clusters$groups[[x$n_clust]])
   } else {
     pop.fac <- pop
   }
 
   if(is.null(pop.fac)) stop("x does not include pre-defined populations, and `pop' is not provided")
 
-
-
-  ## PERFORM PCA ##
-  REDUCEDIM <- is.null(glPca)
-
-  if(REDUCEDIM){ # if no glPca provided
-    maxRank <- min(c(nrow(x), length(show_loci_names(x))))
-    pcaX <- gt_pca(x, center = TRUE, scale = scale, nf=maxRank, loadings=FALSE, returnDotProd = TRUE, ...)
+  if (is.null(n.pca)){
+    if (inherits(x,"gt_pca_clust")){
+      n.pca <- x$clusters$n_pca
+    } else { # use all principal components
+      n.pca <- length(x$eig)
+    }
+  } else { # if n_pca was given, check that it is not too large
+    if(n.pca > ncol(x$scores)) {
+      n.pca <- ncol(x$scores)
+    }
   }
 
-  if(!REDUCEDIM){ # else use the provided glPca object
-    if(is.null(glPca$loadings) & var.contrib) {
-      warning("Contribution of variables requested but glPca object provided without loadings.")
+
+    if(is.null(x$loadings) & var.contrib) {
+      warning("Contribution of variables requested but x' object provided without loadings.")
       var.contrib <- FALSE
     }
-    pcaX <- glPca
-  }
 
-  if(is.null(n.pca)){
-    cumVar <- 100 * cumsum(pcaX$eig)/sum(pcaX$eig)
-  }
-
-
-  ## select the number of retained PC for PCA
-  if(!REDUCEDIM){
-    myCol <- rep(c("black", "lightgrey"), c(ncol(pcaX$scores),length(pcaX$eig)))
-  } else {
-    myCol <- "black"
-  }
-
-  if(is.null(n.pca) & pca.select=="nbEig"){
-    plot(cumVar, xlab="Number of retained PCs", ylab="Cumulative variance (%)", main="Variance explained by PCA", col=myCol)
-    cat("Choose the number PCs to retain (>=1): ")
-    n.pca <- as.integer(readLines(con = getOption('adegenet.testcon'), n = 1))
-  }
-
-  if(is.null(perc.pca) & pca.select=="percVar"){
-    plot(cumVar, xlab="Number of retained PCs", ylab="Cumulative variance (%)", main="Variance explained by PCA", col=myCol)
-    cat("Choose the percentage of variance to retain (0-100): ")
-    nperc.pca <- as.numeric(readLines(con = getOption('adegenet.testcon'), n = 1))
-  }
-
+  # TODO decide what to do with this old code to choose n.pca based on percenta variance
   ## get n.pca from the % of variance to conserve
-  if(!is.null(perc.pca)){
-    n.pca <- min(which(cumVar >= perc.pca))
-    if(perc.pca > 99.999) n.pca <- length(pcaX$eig)
-    if(n.pca<1) n.pca <- 1
-  }
+  # if(!is.null(perc.pca)){
+  #   n.pca <- min(which(cumVar >= perc.pca))
+  #   if(perc.pca > 99.999) n.pca <- length(x$eig)
+  #   if(n.pca<1) n.pca <- 1
+  # }
 
-  if(!REDUCEDIM){
-    if(n.pca > ncol(pcaX$scores)) {
-      n.pca <- ncol(pcaX$scores)
-    }
-  }
-
-
-  ## recompute PCA with loadings if needed
-  if(REDUCEDIM){
-    pcaX <- gt_pca(x, center = TRUE, scale = scale, nf=n.pca, loadings=var.contrib, matDotProd = pcaX$dotProd)
-  }
-
-
-  ## keep relevant PCs - stored in XU
-  N <- nrow(x)
-  X.rank <- sum(pcaX$eig > 1e-14)
-  n.pca <- min(X.rank, n.pca)
-  if(n.pca >= N) n.pca <- N-1
-
-  U <- pcaX$loadings[, 1:n.pca, drop=FALSE] # principal axes
-  XU <- pcaX$scores[, 1:n.pca, drop=FALSE] # principal components
-  XU.lambda <- sum(pcaX$eig[1:n.pca])/sum(pcaX$eig) # sum of retained eigenvalues
+  U <- x$loadings[, 1:n.pca, drop=FALSE] # principal axes
+  XU <- x$scores[, 1:n.pca, drop=FALSE] # principal components
+  XU.lambda <- sum(x$eig[1:n.pca])/sum(x$eig) # sum of retained eigenvalues
   names(U) <- paste("PCA-pa", 1:ncol(U), sep=".")
   names(XU) <- paste("PCA-pc", 1:ncol(XU), sep=".")
 
 
   ## PERFORM DA ##
-  ldaX <- lda(XU, pop.fac, tol=1e-30) # tol=1e-30 is a kludge, but a safe (?) one to avoid fancy rescaling by lda.default
+  ldaX <- MASS::lda(XU, pop.fac, tol=1e-30) # tol=1e-30 is a kludge, but a safe (?) one to avoid fancy rescaling by lda.default
   lda.dim <- sum(ldaX$svd^2 > 1e-10)
   ldaX$svd <- ldaX$svd[1:lda.dim]
   ldaX$scaling <- ldaX$scaling[,1:lda.dim,drop=FALSE]
 
-  if(is.null(n.da)){
-    barplot(ldaX$svd^2, xlab="Linear Discriminants", ylab="F-statistic", main="Discriminant analysis eigenvalues", col=heat.colors(length(levels(pop.fac))) )
-    cat("Choose the number discriminant functions to retain (>=1): ")
-    n.da <- as.integer(readLines(con = getOption('adegenet.testcon'), n = 1))
-  }
+  # if(is.null(n.da)){
+  #   barplot(ldaX$svd^2, xlab="Linear Discriminants", ylab="F-statistic", main="Discriminant analysis eigenvalues", col=heat.colors(length(levels(pop.fac))) )
+  #   cat("Choose the number discriminant functions to retain (>=1): ")
+  #   n.da <- as.integer(readLines(con = getOption('adegenet.testcon'), n = 1))
+  # }
 
   n.da <- min(n.da, length(levels(pop.fac))-1, n.pca, sum(ldaX$svd>1e-10)) # can't be more than K-1 disc. func., or more than n.pca
   n.da <- round(n.da)
@@ -126,32 +86,34 @@ dapc.gen_tibble <- function(x, pop=NULL, n.pca=NULL, n.da=NULL,
   res$call <- match.call()
 
 
-  ## optional: store loadings of variables
-  if(pca.info){
-    res$pca.loadings <- as.matrix(U)
-    res$pca.cent <- snpbin_list_means(x,alleles_as_units =FALSE)
-    if(scale) {
-      res$pca.norm <- sqrt(snpbin_list_vars(x,alleles_as_units = FALSE))
-    } else {
-      res$pca.norm <- rep(1, nLoc(x))
-    }
-    res$pca.eig <- pcaX$eig
-  }
+  #TODO these needs to checked and potentially modified to work without the original output
 
-  ## optional: get loadings of variables
-  if(var.contrib || var.loadings){
-    var.load <- as.matrix(U) %*% as.matrix(ldaX$scaling[,1:n.da,drop=FALSE])
-
-    if(var.contrib){
-      f1 <- function(x){
-        temp <- sum(x*x)
-        if(temp < 1e-12) return(rep(0, length(x)))
-        return(x*x / temp)
-      }
-      res$var.contr <- apply(var.load, 2, f1)
-    }
-    if(var.loadings) res$var.load <- var.load
-  }
+  # ## optional: store loadings of variables
+  # if(pca.info){
+  #   res$pca.loadings <- as.matrix(U)
+  #   res$pca.cent <- snpbin_list_means(x,alleles_as_units =FALSE)
+  #   if(scale) {
+  #     res$pca.norm <- sqrt(snpbin_list_vars(x,alleles_as_units = FALSE))
+  #   } else {
+  #     res$pca.norm <- rep(1, nLoc(x))
+  #   }
+  #   res$pca.eig <- x$eig
+  # }
+  #
+  # ## optional: get loadings of variables
+  # if(var.contrib || var.loadings){
+  #   var.load <- as.matrix(U) %*% as.matrix(ldaX$scaling[,1:n.da,drop=FALSE])
+  #
+  #   if(var.contrib){
+  #     f1 <- function(x){
+  #       temp <- sum(x*x)
+  #       if(temp < 1e-12) return(rep(0, length(x)))
+  #       return(x*x / temp)
+  #     }
+  #     res$var.contr <- apply(var.load, 2, f1)
+  #   }
+  #   if(var.loadings) res$var.load <- var.load
+  # }
 
   class(res) <- "dapc"
   return(res)
--- data-raw/find.clusters.R
+++ data-raw/find.clusters.R
@@ -3,12 +3,12 @@
 #' @export
 ##########################
 
-gt_find_clusters <- function(x, gt_pca=NULL, clust = NULL, n.pca = NULL, n.clust = NULL,
+gt_pca_find_clusters <- function(x = NULL, clust = NULL, n.pca = NULL, n.clust = NULL,
                                    method = c("kmeans", "ward"),
-                                   max.n.clust = round(nrow(x)/10), n.iter = 1e5, n.start = 10,
+                                   max.n.clust = round(nrow(x$scores)/10), n.iter = 1e5, n.start = 10,
                                    scale = FALSE){
 
-  if (is.null(gt_pca) | !inherits(gt_pca, "gt_pca")){
+  if (is.null(x) | !inherits(x, "gt_pca")){
     stop("a 'gt_pca' object is required")
   }
 
@@ -16,13 +16,13 @@ gt_find_clusters <- function(x, gt_pca=NULL, clust = NULL, n.pca = NULL, n.clust
   method <- match.arg(method)
 
   ## SOME GENERAL VARIABLES ##
-  N <- nrow(gt_pca$scores)
+  N <- nrow(x$scores)
   min.n.clust <- 2
   max.n.clust <- max(max.n.clust, 2)
 
   if(is.null(n.pca)){
-    n.pca <- tidy(gt_pca ,matrix="eigenvalues") %>% dplyr::filter(.data$cumulative<0.9) %>% nrow() +1
-    message("'n.pca' was not set: ", n.pca, " components were used")
+    n.pca <- length(anole_pca$eig)
+    message("'n.pca' was not set: all ", n.pca, " components were used")
   }
 
   nbClust <- min.n.clust:max.n.clust
@@ -39,17 +39,17 @@ gt_find_clusters <- function(x, gt_pca=NULL, clust = NULL, n.pca = NULL, n.clust
   for(i in 1:length(nbClust)){
     if (method == "kmeans") {
       ## kmeans clustering (original method)
-      groups_assignments <- kmeans(gt_pca$scores[,seq_len(n.pca)], centers = nbClust[i], iter.max = n.iter, nstart = n.start)$cluster
+      groups_assignments <- kmeans(x$scores[,seq_len(n.pca)], centers = nbClust[i], iter.max = n.iter, nstart = n.start)$cluster
     } else {
       ## ward clustering
-      groups_assignments <- cutree(hclust(dist(gt_pca$scores[,seq_len(n.pca)])^2, method = "ward.D2"), k = nbClust[i])
+      groups_assignments <- cutree(hclust(dist(x$scores[,seq_len(n.pca)])^2, method = "ward.D2"), k = nbClust[i])
     }
-    WSS[i] <- .compute.wss(gt_pca$scores[,seq_len(n.pca)], groups_assignments)
+    WSS[i] <- .compute.wss(x$scores[,seq_len(n.pca)], groups_assignments)
     cluster_list$groups[[i+1]]<- groups_assignments
 
   }
   # compute statistics of goodnees of fit
-  WSS.ori <- sum(apply(gt_pca$scores[,seq_len(n.pca)], 2, function(v) sum((v-mean(v))^2) ))
+  WSS.ori <- sum(apply(x$scores[,seq_len(n.pca)], 2, function(v) sum((v-mean(v))^2) ))
   cluster_list$AIC <- N*log(c(WSS.ori,WSS)/N) + 2*c(1,nbClust)
   cluster_list$BIC <- N*log(c(WSS.ori,WSS)/N) + log(N) *c(1,nbClust)
   cluster_list$WSS <- c(WSS.ori, WSS)
@@ -57,8 +57,9 @@ gt_find_clusters <- function(x, gt_pca=NULL, clust = NULL, n.pca = NULL, n.clust
   cluster_list$groups[[1]]<-setNames(rep(1,N),names(cluster_list$groups[[2]]))
   names(cluster_list$groups)<-seq_len(max.n.clust)
 
-  class(cluster_list) <- c("gt_clusters", class(cluster_list))
-  return(cluster_list)
+  x$clusters <- cluster_list
+  class(x) <- c("gt_pca_clust", class(x))
+  return(x)
 
 }
 
@@ -72,42 +73,45 @@ gt_find_clusters <- function(x, gt_pca=NULL, clust = NULL, n.pca = NULL, n.clust
 }
 
 
-gt_choose_n_clusters <- function(x, stat = c("BIC", "AIC", "WSS"),
+
+##TODO this function should add to the previous object
+gt_pca_clust_choose_n <- function(x, stat = c("BIC", "AIC", "WSS"),
                                  criterion = c("diffNgroup", "min", "goesup",
                                                   "smoothNgoesup", "goodfit"),
                                  quiet=FALSE){
-  if (!inherits(x, "gt_clusters")){
-    stop("'x' should be a 'gt_clusters' object generated with 'gt_find_clusters()'")
+  if (!inherits(x, "gt_pca_clust")){
+    stop("'x' should be a 'gt_pca_clusters' object generated with 'gt_pca_find_clusters()'")
   }
 
   stat <- match.arg(stat)
   criterion <- match.arg(criterion)
 
   if(criterion=="min") {
-    n.clust <- which.min(x[[stat]])
+    n.clust <- which.min(x$clusters[[stat]])
   }
   if(criterion=="goesup") {
     ## temp <- diff(myStat)
     ## n.clust <- which.max( which( (temp-min(temp))<max(temp)/1e4))
-    n.clust <- min(which(diff(x[[stat]])>0))
+    n.clust <- min(which(diff(x$clusters[[stat]])>0))
   }
   if(criterion=="goodfit") {
-    temp <- min(x[[stat]]) + 0.1*(max(x[[stat]]) - min(x[[stat]]))
-    n.clust <- min( which(x[[stat]] < temp))-1
+    temp <- min(x$clusters[[stat]]) + 0.1*(max(x$clusters[[stat]]) - min(x$clusters[[stat]]))
+    n.clust <- min( which(x$clusters[[stat]] < temp))-1
   }
   if(criterion=="diffNgroup") {
-    temp <- cutree(hclust(dist(diff(x[[stat]])), method="ward.D"), k=2)
-    goodgrp <- which.min(tapply(diff(x[[stat]]), temp, mean))
+    temp <- cutree(hclust(dist(diff(x$clusters[[stat]])), method="ward.D"), k=2)
+    goodgrp <- which.min(tapply(diff(x$clusters[[stat]]), temp, mean))
     n.clust <- max(which(temp==goodgrp))+1
   }
   if(criterion=="smoothNgoesup") {
-    temp <- x[[stat]]
-    temp[2:(length(x[[stat]])-1)] <- sapply(1:(length(x[[stat]])-2),
-                                         function(i) mean(x[[stat]][c(i,i+1,i+2)]))
+    temp <- x$clusters[[stat]]
+    temp[2:(length(x$clusters[[stat]])-1)] <- sapply(1:(length(x$clusters[[stat]])-2),
+                                         function(i) mean(x$clusters[[stat]][c(i,i+1,i+2)]))
     n.clust <- min(which(diff(temp)>0))
   }
   if(!quiet){
-    message("Using ",stat, " with criterion ", criterion,":\n")
+    message("Using ",stat, " with criterion ", criterion,": ", n.clust, " clusters")
   }
-  n.clust
+  x$n_clust <- n.clust
+  return(x)
 }
