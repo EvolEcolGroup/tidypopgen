bb133349a492ded39b629c7ca7eb6aa103d83d28
Andrea Manica <am315@cam.ac.uk>
2024-03-18 22:17:51 +0000

    treat vcf like other formats in gen_tibble

--- R/gen_tibble.R
+++ R/gen_tibble.R
@@ -6,14 +6,17 @@
 #' - a string giving the path to a plink BED file, or a [`bigsnpr::bigSNP`]
 #' - a string giving the path to a RDS file storing a `bigSNP` object from
 #' the `bigsnpr` package (usually created with [bigsnpr::snp_readBed()])
+#' - a string giving the path to a vcf file. Note that we currently read the whole
+#' vcf in memory with `vcfR`, so only smallish vcf can be imported. Only biallelic
+#' SNPs will be considered.
 #' - a genotype matrix of dosages (0, 1, 2, NA) giing the dosage of the alternate
 #' allele.
 #' @param indiv_meta a list, data.frame or tibble with compulsory columns 'id'
 #'  and 'population', plus any additional metadata of interest.
 #' @param loci a data.frame or tibble, with compulsory columns 'name', 'chromosome',
 #' and 'position','genetic_dist', 'allele_ref' and 'allele_alt'
-#' @param ... unused (necessary for the method to accept different parameters depending
-#' on `x`)
+#' @param ... if `x` is the name of a vcf file, additional arguments
+#' passed to [vcfR::read.vcfR()]. Otherwise, unused.
 #' @param backingfile the path, including the file name without extension,
 #' for backing files used to store the data (they will be given a .bk
 #' and .RDS automatically). This is not needed if `x` is already an .RDS file.
@@ -35,7 +38,20 @@ gen_tibble <- function(x, ...,  backingfile = NULL, quiet = FALSE) {
 #' @export
 #' @rdname gen_tibble
 gen_tibble.character <- function(x, ..., backingfile = NULL, quiet = FALSE){
-  rlang::check_dots_empty()
+
+  if ((tolower(file_ext(x))=="bed") || (tolower(file_ext(x))=="rds")){
+    rlang::check_dots_empty()
+    gen_tibble_bed_rds(x = x, ..., backingfile = backingfile, quiet = quiet)
+  } else if (tolower(file_ext(x))=="vcf"){
+    gen_tibble_vcf(x = x, ..., backingfile = backingfile, quiet = quiet)
+  } else {
+    stop("file_path should be pointing to a either a PLINK .bed file or a bigSNP .rds file")
+  }
+}
+
+
+gen_tibble_bed_rds <- function(x, ..., backingfile = NULL, quiet = FALSE){
+
   # if it is a bed file, we convert it to a bigsnpr
   if (tolower(file_ext(x))=="bed"){
     if (is.null(backingfile)){
@@ -45,8 +61,6 @@ gen_tibble.character <- function(x, ..., backingfile = NULL, quiet = FALSE){
                                         backingfile = backingfile)
   }  else if (tolower(file_ext(x))=="rds"){
     bigsnp_path <- x
-  } else {
-    stop("file_path should be pointing to a either a PLINK .bed file or a bigSNP .rds file")
   }
 
   bigsnp_obj <- bigsnpr::snp_attach(bigsnp_path)
@@ -69,6 +83,39 @@ gen_tibble.character <- function(x, ..., backingfile = NULL, quiet = FALSE){
   )
 }
 
+gen_tibble_vcf <- function(x, ..., backingfile = NULL, quiet = FALSE) {
+  x <- vcfR::read.vcfR(file = x, verbose = !quiet, ...)
+
+  x <- vcfR::addID(x)
+
+  # create loci table
+  loci <- tibble(name = vcfR::getID(x),
+                 chromosome = vcfR::getCHROM(x),
+                 position = vcfR::getPOS(x),
+                 genetic_dist = 0,
+                 allele_ref = vcfR::getREF(x),
+                 allele_alt = vcfR::getALT(x))
+
+  x <- vcfR::extract.gt(x)
+  x[x=="0|0"] <- 0
+  x[x=="0|1"] <- 1
+  x[x=="1|0"] <- 1
+  x[x=="1|1"] <- 2
+  x[x=="0/0"] <- 0
+  x[x=="0/1"] <- 1
+  x[x=="1/0"] <- 1
+  x[x=="1/1"] <- 2
+  # make sure these are numeric
+  x <- apply(x, 2, as.numeric)
+
+  ind_meta <- tibble(id = colnames(x), population = NA)
+
+  # using the gen_tibble.matrix method
+  gen_tibble(x = x,
+             indiv_meta = ind_meta,
+             loci = loci,
+             backingfile = backingfile)
+}
 
 #' @export
 #' @rdname gen_tibble
