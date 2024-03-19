125bb637ad8e42c70418d4a40bd3ad2428eec6df
Andrea Manica <am315@cam.ac.uk>
2024-03-19 21:06:45 +0000

    faster HWE

--- data-raw/benchmarking/simple benchmark.R
+++ data-raw/benchmarking/simple benchmark.R
@@ -3,10 +3,11 @@ bed_file <- "./data-raw/benchmarking/hgdp_100k.bed"
 hgdp_gt <- gen_tibble(bed_file,  backingfile = tempfile("missing_"))
 gt_save(hgdp_gt)
 
+hdgp_sub <- hgdp_gt %>% select_loci(1:5000)
 tic()
-foo <- hgdp_gt %>% loci_missingness()
+foo <- hdgp_sub %>% loci_hwe_old()
 toc()
 
 tic()
-foo2 <- hgdp_gt %>% loci_missingness()
+foo2 <- hdgp_sub %>% loci_hwe()
 toc()
