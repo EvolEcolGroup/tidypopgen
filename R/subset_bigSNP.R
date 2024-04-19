3063418c1835662a9915db9965cb97fdec93cf6f
Andrea Manica <am315@cam.ac.uk>
2024-04-19 10:36:23 +0100

    fix subsetting for imputed data
    Also avoid making changes to the original datasets when rbinding.

--- R/subset_bigSNP.R
+++ R/subset_bigSNP.R
@@ -19,36 +19,51 @@ subset_bigSNP <- function(X, indiv_indices=NULL, loci_indices=NULL, swap_indices
   if (is.null(loci_indices)){
     loci_indices <- bigstatsr::cols_along(X)
   }
-  new_map <- X$map
-  # first we swap the loci (if necessary)
+  new_map <- X$map[loci_indices, ]
+  new_map_ref <- new_map # a copy to use when swapping alleles
+
+  # save a copy of the FBM with only the columns and rows needed, in the order necessary
+  if (is.null(backingfile)){
+    backingfile <- tempfile()
+  }
+  new_geno <- bigstatsr::big_copy(X$genotypes, ind.row = indiv_indices,
+                                  ind.col = loci_indices, backingfile = backingfile)
+
+  # now we swap the loci (if necessary)
   if (!is.null(swap_indices)){
+    # convert swap indices to the new order
+    swap_indices<- match(swap_indices,loci_indices)
+    swap_indices <- swap_indices[!is.na(swap_indices)]
+
+    # store the old code
+    code_ref <- new_geno$code256
+    # set a code that allows us to see both raw data and imputed data
+    new_geno$code256 <- 1:256
     swap_locus <- function(x){
+      # note that we work on the raw bytes valus
       x_new <- x
-      x_new[x==2]<-0
-      x_new[x==0]<-2
-      x_new[is.na(x_new)]<-3
-      x_new
+      x_new[x==3]<-1
+      x_new[x==1]<-3
+      x_new[x==5]<-7
+      x_new[x==7]<-5
+      as.raw(x_new-1)
     }
     # change the genotypes
     for (i in swap_indices){
-      X$genotypes[,i]<-swap_locus(X$genotypes[,i])
+      new_geno[,i]<-swap_locus(new_geno[,i])
     }
     # now swap the alleles in the map
-    new_map$allele1[swap_indices]<-X$map$allele2[swap_indices]
-    new_map$allele2[swap_indices]<-X$map$allele1[swap_indices]
+    new_map$allele1[swap_indices]<-new_map_ref$allele2[swap_indices]
+    new_map$allele2[swap_indices]<-new_map_ref$allele1[swap_indices]
+    # now reset the code
+    new_geno$code256 <- code_ref
   }
 
-  if (is.null(backingfile)){
-    backingfile <- tempfile(tmpdir = getOption("FBM.dir"))
-  }
-  # save a copy of the FBM with only the columns and rows needed, in the order necessary
-  new_geno <- bigstatsr::big_copy(X$genotypes, ind.row = indiv_indices,
-                      ind.col = loci_indices, backingfile = backingfile)
   # now reassemble
   # Create the bigSNP object
   snp.list <- structure(list(genotypes = new_geno,
                              fam = X$fam[indiv_indices, ],
-                             map = new_map[loci_indices, ]),
+                             map = new_map),
                             class = "bigSNP")
 
   # save it and return the path of the saved object
