9650371c66b7bfa395dbbbbff3925a75f7daefb7
Andrea Manica <am315@cam.ac.uk>
2024-04-26 06:18:20 +0100

    small clean up of pairwise fst

--- R/pop_pairwise_fst.R
+++ R/pop_pairwise_fst.R
@@ -1,7 +1,7 @@
 #' Compute pairwise population Fst
 #'
 #' This function computes pairwise Fst, currently using the formulation by
-#' Hudson, following the reccomandations by Bhatia et al (2013).
+#' Hudson (following the recommendations by Bhatia et al (2013)).
 #'
 #' @references Bhatia G, Patterson N, Sankararaman S, Price AL. Estimating and Interpreting
 #' FST: The Impact of Rare Variants. Genome Research. 2013;23(9):1514–1521.
@@ -10,8 +10,14 @@
 #' or as a single genome wide value obtained by taking the ratio of the mean numerator
 #' and denominator (FALSE, the default).
 #' @param method currently only "Hudson" is implemented
-#' @returns a tibble with each pairwise combination as a row
+#' @returns a tibble with each pairwise combination as a row (if `tidy=TRUE`), else
+#' a matrix
 #' @export
+
+
+# #' @param tidy boolean whether to return a tidy tibble. Default is TRUE, FALSE
+# #' returns a matrix. THIS IS NOT IMPLEMENT YET.
+
 
 pairwise_pop_fst <- function(.x, by_locus=FALSE, method= "Hudson"){
   if (!inherits(.x,"grouped_df")){
@@ -28,7 +34,7 @@ pairwise_pop_fst_hudson <- function(.x, by_locus=FALSE){
 
   message("this function is not properly tested yet!!!")
   message("if you have time, test the results against something like hierfstat and create a unit test")
-  # check matrix(unlist(z, use.names = FALSE), ncol = 10, byrow = TRUE)
+  # matrix(unlist(z, use.names = FALSE), ncol = 10, byrow = TRUE)
   # is known to be faster than do.call(rbind,f)
   # see https://stackoverflow.com/questions/13224553/how-to-convert-a-huge-list-of-vector-to-a-matrix-more-efficiently
   # do not modify the approach used below:
@@ -54,6 +60,7 @@ pairwise_pop_fst_hudson <- function(.x, by_locus=FALSE){
   pairwise_combn <- t(utils::combn(nrow(.group_levels),2))
   numerator <- matrix(NA_real_, nrow = nrow(pairwise_combn), ncol = n_loci)
   denominator <- matrix(NA_real_, nrow = nrow(pairwise_combn), ncol = n_loci)
+  # TODO this could be made faster by parallelising it
   for (i_row in seq_len(nrow(pairwise_combn))){
     pop1 <- pairwise_combn[i_row,1]
     pop2 <- pairwise_combn[i_row,2]
@@ -62,15 +69,15 @@ pairwise_pop_fst_hudson <- function(.x, by_locus=FALSE){
     denominator[i_row,] <- numerator[i_row,] + het[pop1,] + het[pop2,]
   }
 
-  # if we want whole genome level estimates
+  # set up first two columns with pairs of populations
   fst <- tibble(!!paste0(names(.group_levels),"_",1) := .group_levels[pairwise_combn[,1],1] %>% pull(1),
                 !!(paste0(names(.group_levels),"_",2)) := .group_levels[pairwise_combn[,2],1] %>% pull(1))
+  # if we want whole genome level estimates
   if (!by_locus){
     fst_wg_by_row <- function(i, numerator, denominator){
       return(sum(numerator[i,],na.rm=TRUE)/sum(denominator[i,],na.rm=TRUE))
     }
     fst <- fst %>% mutate(value = unlist(lapply(seq_len(nrow(fst)),fst_wg_by_row, numerator, denominator)))
-
     return(fst)
   } else {
     # if we want estimates by locus
