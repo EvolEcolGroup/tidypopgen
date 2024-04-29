1054ed3796067d3a16791957eace204103464338
Evie Carter <eviecarter41@outlook.com>
2024-04-29 14:41:31 +0100

    Update indiv_qc_report to include relatedness

--- R/indiv_qc_report.R
+++ R/indiv_qc_report.R
@@ -3,15 +3,34 @@
 #'#' Return QC information to assess loci (Observed heterozygosity and missingness).
 #'
 #' @param .x a [`gen_tibble`] object.
+#' @param cutoff an optional numeric, a threshold of relatedness for the sample
 #' @param ... further arguments to pass
 #' @returns a tibble with 2 elements: het_obs and missingness
 #' @export
-indiv_qc_report <- function(.x,...){
-  indiv_qc_report <- .x %>% reframe(het_obs = indiv_het_obs(.x),
-                              missingness = indiv_missingness(.x,as_counts=FALSE))
+
+
+indiv_qc_report <- function(.x, cutoff = NULL, ...){
+  rlang::check_dots_empty()
+
+  if(is.null(cutoff)){
+    indiv_qc_report <- .x %>% reframe(het_obs = indiv_het_obs(.x),
+                                      missingness = indiv_missingness(.x,as_counts=FALSE))
+  } else {
+
+    king <- gt_king(.x, as_matrix = TRUE)
+
+    relatives <- filter_high_relatedness(matrix = king, .x = .x, cutoff = cutoff,...)
+
+    indiv_qc_report <- .x %>% reframe(het_obs = indiv_het_obs(.x),
+                                      missingness = indiv_missingness(.x,as_counts=FALSE))
+    indiv_qc_report$to_keep <- relatives[[3]]
+    indiv_qc_report$id <- .x$id
+  }
+
   class(indiv_qc_report) <- c("indiv_qc_report",class(indiv_qc_report))
   indiv_qc_report
 }
+
 
 #' @export
 autoplot.indiv_qc_report <- function(object, type = c("scatter"),miss_threshold = NULL, ...){
@@ -24,18 +43,18 @@ autoplot.indiv_qc_report <- function(object, type = c("scatter"),miss_threshold 
     miss_threshold
   }
 
+
   type <- match.arg(type)
 
   if (type == "scatter") {
     final_plot <- autoplot_indiv_qc_report(object,miss_threshold)
   } else {
-    stop("Invalid type argument. Please choose 'scatter'")
+    stop("Invalid type argument. Please choose from 'scatter' or 'relatedness'")
   }
 
   return(final_plot)
 
 }
-
 
 autoplot_indiv_qc_report <- function(object, miss_threshold = miss_threshold){
 
