2a5dacae0fed1fbbc50d5e8ec1b955aef0f6eebf
Evie Carter <eviecarter41@outlook.com>
2024-05-15 12:57:52 +0100

    addition to loci_ld_clump test

--- tests/testthat/test_loci_ld_clump.R
+++ tests/testthat/test_loci_ld_clump.R
@@ -10,7 +10,7 @@ test_loci <- data.frame(name=paste0("rs",1:6),
                         allele_ref = c("A","T","C","G","C","T"),
                         allele_alt = c("T","C", NA,"C","G","A"))
 
-test_gt <- gen_tibble(x = test_genotypes, indiv_meta = test_indiv_meta, loci = test_loci)
+test_gt <- gen_tibble(x = test_genotypes, indiv_meta = test_indiv_meta, loci = test_loci, quiet = TRUE)
 
 test_that("ld clumping runs",{
 
@@ -19,5 +19,44 @@ test_that("ld clumping runs",{
 
 })
 
+
+
+test_that("loci_ld_clump returns the same as bigsnpr",{
+
+  bedfile <- system.file("extdata/related/families.bed", package="tidypopgen")
+  rds <- bigsnpr::snp_readBed(bedfile, backingfile = tempfile())
+
+  bigsnp <- bigsnpr::snp_attach(rds)
+
+  gen_tbl <- gen_tibble(bedfile, quiet = TRUE,
+                        backingfile = tempfile(), valid_alleles = c("1","2"))
+
+  #also tests imputation
+  bigsnp_imputed <- bigsnpr::snp_fastImputeSimple(bigsnp$genotypes,method = "mode")
+  bigsnp$genotypes <- bigsnp_imputed
+  bigsnpr::snp_save(bigsnp)
+  bigsnp_imputed <- bigsnpr::snp_attach(rds)
+
+
+  gen_tbl_imputed <- gt_impute_simple(gen_tbl, method = "mode")
+  gt_set_imputed(gen_tbl_imputed, TRUE)
+
+  #set up bigsnpr
+  G <- bigsnp_imputed$genotypes
+  POS <- bigsnp_imputed$map$physical.pos
+  CHR <- bigsnp_imputed$map$chromosome
+
+
+  ind.keep <- bigsnpr::snp_clumping(G, infos.chr = CHR, infos.pos = POS,thr.r2 = 0.2)
+  to_keep <- loci_ld_clump(gen_tbl_imputed, thr_r2 = 0.2)
+
+  #convert our output to indices
+  ind <- which(to_keep == TRUE)
+
+  #check they match
+  expect_true(all(ind.keep == ind))
+
+
+})
 
 
