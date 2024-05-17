cb41fad4dd152f290be43b4c272086b923f1afdd
Evie Carter <eviecarter41@outlook.com>
2024-05-17 10:30:34 +0100

    start test for augment_loci

---
+++ tests/testthat/test_augment_loci.R
@@ -0,0 +1,16 @@
+test_that("augment_loci adds to loci_table",{
+  bed_file <- system.file("extdata", "example-missing.bed", package = "bigsnpr")
+  missing_gt <- gen_tibble(bed_file,  backingfile = tempfile("missing_"),quiet=TRUE)
+  missing_gt <- gt_impute_simple(missing_gt)
+  missing_pca <- missing_gt %>% gt_pca_partialSVD()
+
+  #Add loadings to loci table
+  missing_pca_load <- augment_loci(missing_pca, data = missing_gt)
+  expect_true(all(colnames(missing_pca_load) == c(colnames(show_loci(missing_gt)),paste0(".loadingPC", c(1:10)))))
+
+  #Try assigning loadings to object with fewer loci
+  missing_gt_rm <- missing_gt %>% select_loci(c(1:400))
+  expect_error(augment_loci(missing_pca, data = missing_gt_rm),"the loci names in 'data' do not correspond to the loci in the pca object ")
+
+
+})
