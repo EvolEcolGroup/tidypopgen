129bc160ff1ebd5cdc041d8f3db88ebf69f2d040
Andrea Manica <am315@cam.ac.uk>
2024-06-11 10:00:40 +0100

    Implement projection of samples with missing data

--- tests/testthat/test_gt_pca.R
+++ tests/testthat/test_gt_pca.R
@@ -5,8 +5,11 @@ test_that("fit_gt_pca_and_predict",{
                 "You can't have missing")
   missing_gt <- gt_impute_simple(missing_gt, method = "mode")
   missing_pca <- missing_gt %>% gt_pca_partialSVD()
+  # check that predicting on the object is the same as predicting from the full dataset
+  # without imputation to the center (the data are already imputed)
   expect_true(all.equal(predict(missing_pca),
-                        predict(missing_pca, new_data = missing_gt),
+                        predict(missing_pca, new_data = missing_gt,
+                                impute_to_center = FALSE),
                         check.attributes=FALSE))
   # now mismatch the loci table
   missing_gt_edited <- missing_gt
@@ -21,10 +24,14 @@ test_that("fit_gt_pca_and_predict",{
   missing_gt_sub <- missing_gt %>% select_loci(100:450)
   missing_sub_pca <- missing_gt_sub  %>% gt_pca_partialSVD()
   expect_true(all.equal(predict(missing_sub_pca),
-                        predict(missing_sub_pca, new_data = missing_gt),
+                        predict(missing_sub_pca, new_data = missing_gt,
+                                impute_to_center = FALSE),
                         check.attributes=FALSE))
 
 })
+
+# TODO we should test gt_pca_autoSVD(), as the loci have to be subset within
+# the object
 
 test_that("fit_gt_pca_and_predict_splitted_data",{
   bed_file <- system.file("extdata", "example-missing.bed", package = "bigsnpr")
@@ -37,6 +44,9 @@ test_that("fit_gt_pca_and_predict_splitted_data",{
   modern_gt <- gt_impute_simple(modern_gt, method = "mode")
   modern_pca <- modern_gt %>% gt_pca_partialSVD()
   # if we just try to predict, we find that the new data have missing data
-  expect_error(predict(modern_pca, new_data = ancient_gt),
-                        "You can't have")
+  ancient_pred <- predict(modern_pca, new_data = ancient_gt)
+  expect_true(all(dim(ancient_pred)==c(20,10)))
+  # now raise an error if we don't impute to the mean
+  expect_error(predict(modern_pca, new_data = ancient_gt, impute_to_center = FALSE),
+               "You can't have missing values in 'X'")
 })
