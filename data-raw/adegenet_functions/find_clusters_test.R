4b1b0cdae328d01cb51544dfd48339b3ef67d5ad
Andrea Manica <am315@cam.ac.uk>
2024-06-16 05:36:38 +0100

    clean up old files
    [skip ci]

--- data-raw/adegenet_functions/find_clusters_test.R
+++
@@ -1,7 +0,0 @@
-x <- glSim(100,500,500)
-x
-plot(x)
-grp <- find.clusters(x, stat = "BIC") # choose =FALSE
-plot(grp$Kstat, type = "o", xlab = "number of clusters (K)",
-     ylab = "BIC",
-     main = "find.clusters on a genlight object\n(two groups)")
