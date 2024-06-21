5de1f015a3e540bdc88b0b11acc624f2c264fdb7
Andrea Manica <am315@cam.ac.uk>
2024-06-21 16:30:18 +0100

    test pca predict on subset

--- tests/testthat/test_gt_pca.R
+++ tests/testthat/test_gt_pca.R
@@ -10,3 +10,19 @@ test_that("fit_gt_pca_and_predict",{
                         check.attributes=FALSE))
 })
 
+test_that("fit_gt_pca_and_predict subset",{
+  bed_file <- system.file("extdata", "example-missing.bed", package = "bigsnpr")
+  missing_gt <- gen_tibble(bed_file,  backingfile = tempfile("missing_"),quiet=TRUE)
+  missing_removed_gt <- missing_gt[1:30,]
+  missing_gt <- missing_gt[-(1:30),]
+  missing_gt <- gt_impute_simple(missing_gt, method = "mode")
+  missing_pca <- missing_gt %>% gt_pca_partialSVD()
+  expect_true(all.equal(predict(missing_pca),
+                        predict(missing_pca, new_data = missing_gt),
+                        check.attributes=FALSE))
+ predict(missing_pca, new_data = missing_removed_gt)
+
+})
+
+
+
