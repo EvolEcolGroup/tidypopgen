03e099771048759f15ee0dca500f9fc300d5b073
eviecarter33 <eviecarter41@outlook.com>
2024-10-11 11:37:56 +0100

    fix paths

--- tests/testthat/test_q_matrix.R
+++ tests/testthat/test_q_matrix.R
@@ -1,6 +1,6 @@
 test_that("q_matrix reads q_files",{
   #read a single .Q
-  Q_path <- system.file("/inst/extdata/anolis/anolis_ld_run1.3.Q", package = "tidypopgen")
+  Q_path <- system.file("/extdata/anolis/anolis_ld_run1.3.Q", package = "tidypopgen")
   anolis_q_k3 <- q_matrix(Q_path)
   expect_true(inherits(anolis_q_k3,"q_matrix"))
 
@@ -15,20 +15,19 @@ test_that("q_matrix reads q_files",{
   expect_true(inherits(anolis_q_k3_mat,"q_matrix"))
 
   #read multiple .Q
-  #q_folder <- file.path("/inst/extdata/anolis")
   package_path <- find.package("tidypopgen")
-  folder <- "inst/extdata/anolis"
+  folder <- "/extdata/anolis"
   q_folder <- file.path(package_path, folder)
   anolis_q <- q_matrix(q_folder)
   expect_true(inherits(anolis_q,"q_matrix_list"))
   expect_true(inherits(anolis_q[[1]][[1]],"q_matrix"))
 
   #check errors
-  P_path <- system.file("/inst/extdata/anolis/anolis_ld_run1.3.P", package = "tidypopgen")
+  P_path <- system.file("/extdata/anolis/anolis_ld_run1.3.P", package = "tidypopgen")
   expect_error(q_matrix(P_path),"Input file does not end in '.Q'")
   non_path <- "an/invalid/path"
   expect_error(q_matrix(non_path),"Input is not a valid dataframe, file or directory")
-  path_no_q <- system.file("/inst/extdata/", package = "tidypopgen")
+  path_no_q <- system.file("/extdata/", package = "tidypopgen")
   expect_error(q_matrix(path_no_q),"No .Q files found in the directory")
 })
 
