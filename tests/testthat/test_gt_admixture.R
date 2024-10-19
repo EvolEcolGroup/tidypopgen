29db179267c5926b52370c4a610ea73ddaa2f94f
Andrea Manica <am315@cam.ac.uk>
2024-10-19 11:22:04 +0100

    clean up

--- tests/testthat/test_gt_admixture.R
+++ tests/testthat/test_gt_admixture.R
@@ -1,4 +1,7 @@
 test_that("gt_admixture", {
+  # skip if admixture is not installed (TODO we should also check if we have it through reticulate)
+  skip_if ((system2("which", args = "admixture") != 0))
+
   bed_file <- system.file("extdata", "example-missing.bed", package = "bigsnpr")
   res_admix <- gt_admixture(bed_file, k = 3)
 
