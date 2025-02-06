116217512987790325975b730d16a3b70484485b
eviecarter33 <eviecarter41@outlook.com>
2025-02-06 16:56:22 +0000

    additional tests for case of variant missing for all in a population, WC method not working

--- tests/testthat/test_pairwise_pop_fst.R
+++ tests/testthat/test_pairwise_pop_fst.R
@@ -81,6 +81,37 @@ test_that("pairwise_pop_fst weir_cockerham",{
   # read in output
   wc_scikit_mono <- as.numeric(readLines(system.file("/extdata/fst_scikit-allel/fst_wc_monomorphic.txt", package = "tidypopgen")))
   expect_equal(wc_tidypopgen_mono$value, wc_scikit_mono)
+  ######################
+  # test with 1st loci missing for all individuals in 1st population
+  ######################
+  test_genotypes <- rbind(c(NA,1,0,1,1,0),
+                          c(NA,1,0,NA,0,0),
+                          c(NA,NA,0,0,1,1),
+                          c(2,0,0,1,0,0),
+                          c(1,2,0,1,2,1),
+                          c(2,0,0,0,NA,1),
+                          c(2,1,1,0,1,NA))
+  test_gt <- gen_tibble(x = test_genotypes, loci = test_loci, indiv_meta = test_indiv_meta, quiet = TRUE)
+  test_gt <- test_gt %>% dplyr::group_by(population)
+  wc_tidypopgen_fixed_var1 <- test_gt %>% pairwise_pop_fst(method="WC84")
+  # the variant should be ignored, so fst should be equal to fst without this variant
+  test_genotypes <- rbind(c(1,0,1,1,0),
+                          c(1,0,NA,0,0),
+                          c(NA,0,0,1,1),
+                          c(0,0,1,0,0),
+                          c(2,0,1,2,1),
+                          c(0,0,0,NA,1),
+                          c(1,1,0,1,NA))
+  test_loci <- data.frame(name=paste0("rs",1:5),
+                          chromosome=paste0("chr",c(1,1,1,2,2)),
+                          position=as.integer(c(5,65,343,23,456)),
+                          genetic_dist = as.double(rep(0,5)),
+                          allele_ref = c("T","C","G","C","T"),
+                          allele_alt = c("C", NA,"C","G","A"))
+  test_gt <- gen_tibble(x = test_genotypes, loci = test_loci, indiv_meta = test_indiv_meta, quiet = TRUE)
+  test_gt <- test_gt %>% dplyr::group_by(population)
+  wc_tidypopgen_missing_var1 <- test_gt %>% pairwise_pop_fst(method="WC84")
+  expect_equal(wc_tidypopgen_fixed_var1$value, wc_tidypopgen_missing_var1$value)
 })
 
 
@@ -112,7 +143,9 @@ test_that("pairwise_pop_fst hudson",{
   hudson_scikit <- as.numeric(readLines(system.file("/extdata/fst_scikit-allel/fst_hudson.txt", package = "tidypopgen")))
   expect_equal(hudson_tidypopgen$value, hudson_scikit)
 
-  ########### test with a monomorphic loci
+  ######################
+  # test with a monomorphic loci
+  ######################
   test_genotypes <- rbind(c(2,1,0,1,1,0),
                           c(2,1,0,NA,0,0),
                           c(2,NA,0,0,1,1),
@@ -129,6 +162,37 @@ test_that("pairwise_pop_fst hudson",{
   hudson_scikit_mono <- as.numeric(readLines(system.file("/extdata/fst_scikit-allel/fst_hudson_monomorphic.txt", package = "tidypopgen")))
   expect_equal(hudson_tidypopgen_mono$value, hudson_scikit_mono)
 
+  ######################
+  # test with 1st loci missing for all individuals in 1st population
+  ######################
+  test_genotypes <- rbind(c(NA,1,0,1,1,0),
+                          c(NA,1,0,NA,0,0),
+                          c(NA,NA,0,0,1,1),
+                          c(2,0,0,1,0,0),
+                          c(1,2,0,1,2,1),
+                          c(2,0,0,0,NA,1),
+                          c(2,1,1,0,1,NA))
+  test_gt <- gen_tibble(x = test_genotypes, loci = test_loci, indiv_meta = test_indiv_meta, quiet = TRUE)
+  test_gt <- test_gt %>% dplyr::group_by(population)
+  hudson_tidypopgen_fixed_var1 <- test_gt %>% pairwise_pop_fst(method="Hudson")
+  # the variant should be ignored, so fst should be equal to fst without that variant
+  test_genotypes <- rbind(c(1,0,1,1,0),
+                          c(1,0,NA,0,0),
+                          c(NA,0,0,1,1),
+                          c(0,0,1,0,0),
+                          c(2,0,1,2,1),
+                          c(0,0,0,NA,1),
+                          c(1,1,0,1,NA))
+  test_loci <- data.frame(name=paste0("rs",1:5),
+                          chromosome=paste0("chr",c(1,1,1,2,2)),
+                          position=as.integer(c(5,65,343,23,456)),
+                          genetic_dist = as.double(rep(0,5)),
+                          allele_ref = c("T","C","G","C","T"),
+                          allele_alt = c("C", NA,"C","G","A"))
+  test_gt <- gen_tibble(x = test_genotypes, loci = test_loci, indiv_meta = test_indiv_meta, quiet = TRUE)
+  test_gt <- test_gt %>% dplyr::group_by(population)
+  hudson_tidypopgen_missing_var1 <- test_gt %>% pairwise_pop_fst(method="Hudson")
+  expect_equal(hudson_tidypopgen_fixed_var1$value, hudson_tidypopgen_missing_var1$value)
 })
 
 
