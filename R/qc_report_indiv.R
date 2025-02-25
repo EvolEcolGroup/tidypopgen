71d8e9d8253e7bd717dad8c2bb3b1982f16074bd
eviecarter33 <eviecarter41@outlook.com>
2025-02-25 16:43:17 +0000

    fix grouped implementation of qc_loci_report and add test

--- R/qc_report_indiv.R
+++ R/qc_report_indiv.R
@@ -1,13 +1,16 @@
-#' Create a Quality Control report for individuals
+#'Create a Quality Control report for individuals
 #'
-#'#' Return QC information to assess loci (Observed heterozygosity and missingness).
+#'Return QC information to assess loci (Observed heterozygosity and
+#'missingness).
 #'
-#' @param .x either a [`gen_tibble`] object or a grouped [`gen_tibble`] (as obtained by using [dplyr::group_by()])
-#' @param kings_threshold an optional numeric, a threshold of relatedness for the sample
-#' @param ... further arguments to pass
-#' @returns a tibble with 2 elements: het_obs and missingness
-#' @rdname qc_report_indiv
-#' @export
+#'@param .x either a [`gen_tibble`] object or a grouped [`gen_tibble`] (as
+#'  obtained by using [dplyr::group_by()])
+#'@param kings_threshold an optional numeric, a threshold of relatedness for the
+#'  sample
+#'@param ... further arguments to pass
+#'@returns a tibble with 2 elements: het_obs and missingness
+#'@rdname qc_report_indiv
+#'@export
 qc_report_indiv <- function(.x, ...) {
   UseMethod("qc_report_indiv", .x)
 }
@@ -15,101 +18,89 @@ qc_report_indiv <- function(.x, ...) {
 
 #' @export
 #' @rdname qc_report_indiv
-qc_report_indiv.tbl_df <- function(.x, kings_threshold = NULL, ...){
+qc_report_indiv.tbl_df <- function(.x, kings_threshold = NULL, ...) {
   rlang::check_dots_empty()
 
-  if(is.null(kings_threshold)){
-    qc_report_indiv <- .x %>% reframe(het_obs = indiv_het_obs(.x),
-                                      missingness = indiv_missingness(.x,as_counts=FALSE))
+  if (is.null(kings_threshold)) {
+    qc_report_indiv <- .x %>%
+      reframe(het_obs = indiv_het_obs(.x),
+              missingness = indiv_missingness(.x, as_counts = FALSE))
   } else {
 
     # calculate the kinship matrix
     king <- pairwise_king(.x, as_matrix = TRUE)
 
-    relatives <- filter_high_relatedness(matrix = king, .x = .x, kings_threshold = kings_threshold,...)
+    relatives <- filter_high_relatedness(matrix = king, .x = .x,
+                                         kings_threshold = kings_threshold, ...)
 
-    qc_report_indiv <- .x %>% reframe(het_obs = indiv_het_obs(.x),
-                                      missingness = indiv_missingness(.x,as_counts=FALSE))
+    qc_report_indiv <- .x %>%
+      reframe(het_obs = indiv_het_obs(.x),
+              missingness = indiv_missingness(.x, as_counts = FALSE))
     qc_report_indiv$to_keep <- relatives[[3]]
     qc_report_indiv$id <- .x$id
     attr(qc_report_indiv$to_keep, "king") <- king
 
   }
-  class(qc_report_indiv) <- c("qc_report_indiv",class(qc_report_indiv))
+  class(qc_report_indiv) <- c("qc_report_indiv", class(qc_report_indiv))
   qc_report_indiv
 }
 
 
 #' @export
 #' @rdname qc_report_indiv
-qc_report_indiv.grouped_df <- function(.x, kings_threshold = NULL, ...){
+qc_report_indiv.grouped_df <- function(.x, kings_threshold = NULL, ...) {
   rlang::check_dots_empty()
 
-  if(is.null(kings_threshold)){
-    qc_report_indiv <- .x %>% ungroup() %>% reframe(het_obs = indiv_het_obs(.x),
-                                      missingness = indiv_missingness(.x,as_counts=FALSE))
+  if (is.null(kings_threshold)) {
+    qc_report_indiv <- .x %>%
+      ungroup() %>%
+      reframe(het_obs = indiv_het_obs(.x),
+              missingness = indiv_missingness(.x, as_counts = FALSE))
   } else {
 
-  # calculate the kinship matrix
-  #king <- pairwise_king(.x, as_matrix = TRUE)
+  # find grouping levels
+  grouping_order <- group_keys(.x)
 
   # calculate the kinship matrix for each population
   king <- .x %>% group_map(~pairwise_king(.x, as_matrix = TRUE))
+  names(king) <- as.matrix(grouping_order)[, 1]
 
   # create relatives, an empty list
   relatives <- vector("list")
 
   # remove individuals using filter_high_relatedness for each population
-  indivs_to_keep <- function(relatives,king){
-    for(i in 1:length(king)){
-      output <- filter_high_relatedness(king[[i]],kings_threshold = kings_threshold)[[3]]
+  indivs_to_keep <- function(relatives, king) {
+    for (i in 1:length(king)){
+      output <- filter_high_relatedness(king[[i]],
+                                        kings_threshold = kings_threshold)
       # add the output to the list
-      relatives[[i]] <- output
+      relatives[[i]] <- output[[3]]
+      names(relatives)[i] <- names(king)[i]
     }
     return(relatives)
   }
 
-  relatives <- indivs_to_keep(relatives,king)
+  relatives <- indivs_to_keep(relatives, king)
 
-  #relatives <- .x %>%
-  #  group_map(~ {
-      # Subset the matrix for the current group
-      #group_matrix <- king[rownames(king) %in% .x$id, colnames(king) %in% .x$id]
-
-      # Apply filter_high_relatedness to the group-specific matrix
-   #   filter_high_relatedness(matrix = group_matrix, .x = .x, kings_threshold = kings_threshold)
-  #  })
-
-
-  # Combine all retained IDs (element `1` in each sublist)
-  #all_retained_ids <- unlist(lapply(relatives, function(x) x[[1]]))
-
-  # Combine all removed IDs (element `2` in each sublist)
-  #all_removed_ids <- unlist(lapply(relatives, function(x) x[[2]]))
-
-  # Combine the boolean TRUE/FALSE values (element `3` in each sublist)
-  #all_flags <- unlist(lapply(relatives, function(x) x[[3]]))
-  all_flags <- unlist(relatives)
-
-  # The resulting lists are now of length = n rows, not split by groups
-  #relatives <- list(retained = all_retained_ids,
-  #                  removed = all_removed_ids,
-  #                  flags = all_flags)
-
-  # find the group variable of .x
-  #group_var <- attr(.x, "group_vars")
-
-  qc_report_indiv <- .x %>% ungroup() %>% reframe(het_obs = indiv_het_obs(.x),
-                                                  missingness = indiv_missingness(.x,as_counts=FALSE))
-  qc_report_indiv$to_keep <- all_flags
-  qc_report_indiv$id <- .x$id
+  # create qc_report_indiv
+  qc_report_indiv <- .x %>%
+    ungroup() %>%
+    reframe(het_obs = indiv_het_obs(.x),
+            missingness = indiv_missingness(.x, as_counts = FALSE)) %>%
+    mutate(id = .x$id)
+  # re-group according to original .x grouping
   qc_report_indiv$group <- .x[[dplyr::group_vars(.x)]]
+  # reorder relatives
+  relatives <- relatives[unique(qc_report_indiv$group)]
+  all_flags <- unlist(relatives)
+  # add to_remove
+  qc_report_indiv$to_keep <- as.vector(all_flags)
   attr(qc_report_indiv$to_keep, "king") <- king
 
   }
 
-  class(qc_report_indiv) <- c("qc_report_indiv",class(qc_report_indiv))
-  qc_report_indiv
+  class(qc_report_indiv) <- c("qc_report_indiv", class(qc_report_indiv))
+  return(qc_report_indiv)
 
 }
 
@@ -123,23 +114,26 @@ qc_report_indiv.grouped_df <- function(.x, kings_threshold = NULL, ...){
 #' individuals.
 #' - `relatedness`: a histogram of paired kinship coefficients
 #'
-#' `autoplot` produces simple plots to quickly inspect an object. They are
-#' not customisable; we recommend that you use `ggplot2` to produce publication
+#' `autoplot` produces simple plots to quickly inspect an object. They are not
+#' customisable; we recommend that you use `ggplot2` to produce publication
 #' ready plots.
 #'
 #' @param object an object of class `qc_report_indiv`
 #' @param type the type of plot (`scatter`,`relatedness`)
 #' @param miss_threshold a threshold for the accepted rate of missingness within
-#' individuals
-#' @param kings_threshold an optional numeric, a threshold of relatedness for the sample
+#'   individuals
+#' @param kings_threshold an optional numeric, a threshold of relatedness for
+#'   the sample
 #' @param ... not currently used.
 #' @returns a `ggplot2` object
 #' @export
-autoplot.qc_report_indiv <- function(object, type = c("scatter", "relatedness"),miss_threshold = NULL, kings_threshold = kings_threshold, ...){
+autoplot.qc_report_indiv <- function(object, type = c("scatter", "relatedness"),
+                                     miss_threshold = NULL,
+                                     kings_threshold = kings_threshold, ...) {
 
   rlang::check_dots_empty()
 
-  miss_threshold <- if(is.null(miss_threshold)){
+  miss_threshold <- if (is.null(miss_threshold)) {
     0.05
   } else {
     miss_threshold
@@ -148,9 +142,11 @@ autoplot.qc_report_indiv <- function(object, type = c("scatter", "relatedness"),
   type <- match.arg(type)
 
   if (type == "scatter") {
-    final_plot <- autoplot_qc_report_indiv(object,miss_threshold = miss_threshold)
+    final_plot <- autoplot_qc_report_indiv(object,
+                                           miss_threshold = miss_threshold)
   } else if (type == "relatedness") {
-    final_plot <- autoplot_qc_report_indiv_king(object,kings_threshold = kings_threshold)
+    final_plot <- autoplot_qc_report_indiv_king(object,
+                                                kings_threshold = kings_threshold)
   } else {
     stop("Invalid type argument. Please choose from 'scatter' or 'relatedness'")
   }
@@ -159,26 +155,33 @@ autoplot.qc_report_indiv <- function(object, type = c("scatter", "relatedness"),
 
 }
 
-autoplot_qc_report_indiv <- function(object, miss_threshold = miss_threshold){
+autoplot_qc_report_indiv <- function(object, miss_threshold = miss_threshold) {
 
   miss_threshold <- miss_threshold
 
   mean_val <- mean(object$het_obs)
   sd_val <- stats::sd(object$het_obs)
 
-  upper <- mean_val + 3*(sd_val)
-  lower <- mean_val - 3*(sd_val)
+  upper <- mean_val + 3 * (sd_val)
+  lower <- mean_val - 3 * (sd_val)
 
-  mid_upper <- mean_val + 2*(sd_val)
-  mid_lower <- mean_val - 2*(sd_val)
+  mid_upper <- mean_val + 2 * (sd_val)
+  mid_lower <- mean_val - 2 * (sd_val)
 
-  final_plot <- ggplot2::ggplot(object,ggplot2::aes(x=.data$missingness,y=.data$het_obs))+ggplot2::geom_point()+ggplot2::labs(x="Missingness",y="Observed Heterozygosity",title="Heterozygosity and missingness by individual")+ ggplot2::geom_vline(xintercept= miss_threshold, lty=2, col="red")+ggplot2::geom_hline(yintercept = c(upper,lower,mid_upper,mid_lower),lty=2,col="blue")
-
+  final_plot <- ggplot2::ggplot(object, ggplot2::aes(x = .data$missingness,
+                                                     y = .data$het_obs)) +
+    ggplot2::geom_point() +
+    ggplot2::labs(x = "Missingness", y = "Observed Heterozygosity",
+                  title = "Heterozygosity and missingness by individual") +
+    ggplot2::geom_vline(xintercept = miss_threshold, lty = 2, col = "red") +
+    ggplot2::geom_hline(yintercept = c(upper, lower, mid_upper, mid_lower),
+                        lty = 2, col = "blue")
+  return(final_plot)
 }
 
-autoplot_qc_report_indiv_king <- function(object, kings_threshold = kings_threshold){
+autoplot_qc_report_indiv_king <- function(object, kings_threshold = kings_threshold) {
 
-  if(inherits(attr(object$to_keep, "king"), "matrix")|inherits(attr(object$to_keep, "king"), "array")){
+  if (inherits(attr(object$to_keep, "king"), "matrix") || inherits(attr(object$to_keep, "king"), "array")) {
 
     king <- as.data.frame(attr(object$to_keep, "king"))
     num_samples <- nrow(king)
@@ -188,7 +191,7 @@ autoplot_qc_report_indiv_king <- function(object, kings_threshold = kings_thresh
     king <- tidyr::pivot_longer(king, cols = !row, names_to = "Column",
                                 values_to = "Value")
     king <- dplyr::filter(king, king$row < king$Column)
-    colnames(king) <- c("ID1","ID2","kinship")
+    colnames(king) <- c("ID1", "ID2", "kinship")
 
     #remove duplication from the new df
     king_sorted <- king %>%
@@ -204,30 +207,30 @@ autoplot_qc_report_indiv_king <- function(object, kings_threshold = kings_thresh
     king_sorted <- king_sorted %>% filter(.data$ID1 != .data$ID2)
 
     #add a check for correct number of pairs
-    total_pairs <- num_samples * (num_samples -1)/2
+    total_pairs <- num_samples * (num_samples - 1) / 2
 
-    if (total_pairs != nrow(king_sorted)){
+    if (total_pairs != nrow(king_sorted)) {
       stop("Relatedness matrix must be symmetric ")
     }
 
-    p <- ggplot2::ggplot(king_sorted, ggplot2::aes(x=.data$kinship)) +
+    p <- ggplot2::ggplot(king_sorted, ggplot2::aes(x = .data$kinship)) +
       ggplot2::geom_histogram(bins = 40) +
-      ggplot2::labs(x="KING robust kinship estimator",y = "Number of pairs",
+      ggplot2::labs(x = "KING robust kinship estimator", y = "Number of pairs",
                     title = "Distribution of paired kinship coefficients") +
-      ggplot2::geom_vline(xintercept = kings_threshold, lty=2, col="red")
+      ggplot2::geom_vline(xintercept = kings_threshold, lty = 2, col = "red")
 
 
-  } else if(inherits(attr(object$to_keep, "king"), "list")){ # by group
+  } else if (inherits(attr(object$to_keep, "king"), "list")) { # by group
 
     # create a plotting function as above
-    hist_plot <- function(object){
+    hist_plot <- function(object) {
       king <- as.data.frame(object)
       num_samples <- nrow(king)
       king$row <- colnames(king)
       king <- tidyr::pivot_longer(king, cols = !row, names_to = "Column",
                                   values_to = "Value")
       king <- dplyr::filter(king, king$row < king$Column)
-      colnames(king) <- c("ID1","ID2","kinship")
+      colnames(king) <- c("ID1", "ID2", "kinship")
       king_sorted <- king %>%
         mutate(
           row_min = pmin(.data$ID1, .data$ID2),
@@ -237,15 +240,16 @@ autoplot_qc_report_indiv_king <- function(object, kings_threshold = kings_thresh
         distinct(.data$row_min, .data$row_max, .keep_all = TRUE) %>%
         rename("ID1" = .data$row_min, "ID2" = .data$row_max)
       king_sorted <- king_sorted %>% filter(.data$ID1 != .data$ID2)
-      total_pairs <- num_samples * (num_samples -1)/2
-      if (total_pairs != nrow(king_sorted)){
+      total_pairs <- num_samples * (num_samples - 1) / 2
+      if (total_pairs != nrow(king_sorted)) {
         stop("Relatedness matrix must be symmetric ")
       }
-      p <- ggplot2::ggplot(king_sorted, ggplot2::aes(x=.data$kinship)) +
+      p <- ggplot2::ggplot(king_sorted, ggplot2::aes(x = .data$kinship)) +
         ggplot2::geom_histogram(bins = 40) +
-        ggplot2::labs(x="KING robust kinship estimator",y = "Number of pairs",
+        ggplot2::labs(x = "KING robust kinship estimator",
+                      y = "Number of pairs",
                       title = "Distribution of paired kinship coefficients") +
-        ggplot2::geom_vline(xintercept = kings_threshold, lty=2, col="red")
+        ggplot2::geom_vline(xintercept = kings_threshold, lty = 2, col = "red")
 
       return(p)
     }
@@ -256,5 +260,4 @@ autoplot_qc_report_indiv_king <- function(object, kings_threshold = kings_thresh
   }
 
   return(p)
-  }
-
+}
--- tests/testthat/test_qc_report_indiv.R
+++ tests/testthat/test_qc_report_indiv.R
@@ -20,4 +20,49 @@ test_that("qc_report_indiv filters relatives within groups",{
   expect_equal(length(myplots),2)
   expect_true(all(grouped$to_keep))
 
+  families$population <- c(rep("pop1",6), rep("pop2",4),rep("pop3",2))
+
+  # subset families to each population
+  pop1 <- filter(families, population == "pop1")
+  pop2 <- filter(families, population == "pop2")
+  pop3 <- filter(families, population == "pop3")
+
+  # run separate qc reports
+  pop1_report <- qc_report_indiv(pop1, kings_threshold = 0.2)
+  pop2_report <- qc_report_indiv(pop2, kings_threshold = 0.2)
+  pop3_report <- qc_report_indiv(pop3, kings_threshold = 0.2)
+
+  families <- families %>% group_by(population)
+  grouped <- qc_report_indiv(families, kings_threshold = 0.2)
+  # The correct individuals are removed
+  expect_equal(as.vector(grouped$to_keep), c(pop1_report$to_keep,pop2_report$to_keep,pop3_report$to_keep))
 })
+
+test_that("qc_report_indiv$to_keep is correctly ordered",{
+  # Create gentibble
+  bed_path <- system.file("extdata/related/families.bed", package = "tidypopgen")
+  families_bigsnp_path <- bigsnpr::snp_readBed(bed_path, backingfile = tempfile())
+  families <- gen_tibble(families_bigsnp_path, quiet = TRUE, valid_alleles = c("1","2"))
+  # create a population
+  families$population <- c(rep("pop1",6), rep("GroupA",6))
+  # make it a factor
+  families$population <- as.factor(families$population)
+  # GroupA is level 1, and pop1 is level 2
+  expect_equal(levels(families$population), c("GroupA","pop1"))
+  # But GroupA are individuals 6:12 and pop1 are individuals 1:6
+  # Grouping levels are therefore not in line with order in $population
+  families <- families %>% group_by(population)
+  # qc_report_indiv should return the correct order
+  grouped <- qc_report_indiv(families, kings_threshold = 0.2)
+
+  families <- families %>% ungroup()
+  # subset families to each population
+  pop1 <- filter(families, population == "pop1")
+  GroupA <- filter(families, population == "GroupA")
+  # run separate qc reports
+  pop1_report <- qc_report_indiv(pop1, kings_threshold = 0.2)
+  GroupA_report <- qc_report_indiv(GroupA, kings_threshold = 0.2)
+  # The correct individuals are removed
+  expect_equal(as.vector(grouped$to_keep), c(pop1_report$to_keep,GroupA_report$to_keep))
+})
+
