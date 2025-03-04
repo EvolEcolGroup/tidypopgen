4b3c3be0cd1caaf416c78a3043419060db8406b1
eviecarter33 <eviecarter41@outlook.com>
2025-03-04 16:39:02 +0000

    style and lint gt_admix and gt_as functions

--- R/gt_as_geno_lea.R
+++ R/gt_as_geno_lea.R
@@ -1,34 +1,35 @@
 #' Convert a `gentibble` to a .geno file for sNMF from the LEA package
 #'
-#' This function writes a .geno file fom a [`gen_tibble`]. Unless a file path is given,
-#' a file with suffix .geno is written in the same location as the .rds and .bk
-#' files that underpin the [`gen_tibble`].
+#' This function writes a .geno file fom a [`gen_tibble`]. Unless a file path is
+#' given, a file with suffix .geno is written in the same location as the .rds
+#' and .bk files that underpin the [`gen_tibble`].
 #'
-#' NOTE that we currently read all the data into memory to write the file, so this
-#' function is not suitable for very large datasets.
+#' NOTE that we currently read all the data into memory to write the file, so
+#' this function is not suitable for very large datasets.
 #'
 #' @param x a [`gen_tibble`]
 #' @param file the .geno filename with a path, or NULL (the default) to use the
-#' location of the backing files.
+#'   location of the backing files.
 #' @returns the path of the .geno file
 #' @export
 #'
 
 # Write a geno file (as used by LEA)
-gt_as_geno_lea <- function(x, file = NULL){
-  if (is.null(file)){
-    file <- sub_rds(gt_get_file_names(x)[1],".geno")
+gt_as_geno_lea <- function(x, file = NULL) {
+  if (is.null(file)) {
+    file <- sub_rds(gt_get_file_names(x)[1], ".geno")
   }
-  if (file_ext(file)!="geno"){
-    file <- paste0(file,".geno")
+  if (file_ext(file) != "geno") {
+    file <- paste0(file, ".geno")
   }
   # TODO this should be done in chunks for larger files
   geno_matrix <- t(x %>% show_genotypes())
-  geno_matrix[is.na(geno_matrix)]<-9
-  utils::write.table(geno_matrix ,sep="",
-              file = file, row.names = FALSE,
-              col.names = FALSE)
+  geno_matrix[is.na(geno_matrix)] <- 9
+  utils::write.table(geno_matrix,
+    sep = "",
+    file = file, row.names = FALSE,
+    col.names = FALSE
+  )
   # return the file name
   return(file)
 }
-
--- R/gt_as_vcf.R
+++ R/gt_as_vcf.R
@@ -4,94 +4,112 @@
 #'
 #' @param x a [`gen_tibble`], with population coded as 'population'
 #' @param file the .vcf file name with a path, or NULL (the default) to use the
-#' location of the backing files.
-#' @param chunk_size the number of loci
-#'  processed at a time. Automatically set if left to NULL
-#' @param overwrite logical, should the file be overwritten if it already exists?
+#'   location of the backing files.
+#' @param chunk_size the number of loci processed at a time. Automatically set
+#'   if left to NULL
+#' @param overwrite logical, should the file be overwritten if it already
+#'   exists?
 #' @returns the path of the .vcf file
 #' @export
 #'
 
-gt_as_vcf <- function(x, file = NULL, chunk_size = NULL, overwrite = FALSE){
+gt_as_vcf <- function(x, file = NULL, chunk_size = NULL, overwrite = FALSE) {
   # check that x is a gen_tibble
   stopifnot_gen_tibble(x)
 
   # vcf writing currently only works for diploid data
   stopifnot_diploid(x$genotypes)
 
-  if (is.null(file)){
-    file <- bigstatsr::sub_bk(attr(x$genotypes,"bigsnp")$genotypes$backingfile,paste0(".vcf"))
+  if (is.null(file)) {
+    file <- bigstatsr::sub_bk(
+      attr(x$genotypes, "bigsnp")$genotypes$backingfile,
+      paste0(".vcf")
+    )
   }
-  if (file_ext(file)!="vcf"){
-    file <- paste0(file,".vcf")
+  if (file_ext(file) != "vcf") {
+    file <- paste0(file, ".vcf")
   }
-  if (!overwrite && file.exists(file)){
-    stop("file ", file," already exists; use 'overwrite=TRUE' to overwrite it")
+  if (!overwrite && file.exists(file)) {
+    stop("file ", file, " already exists; use 'overwrite=TRUE' to overwrite it")
   }
 
   # if chunk is null, get the best guess of an efficient approach
-  if (is.null(chunk_size)){
+  if (is.null(chunk_size)) {
     chunk_size <- bigstatsr::block_size(nrow(x))
   }
 
   # set up chunks
   chunks_vec <- rep(chunk_size, floor(count_loci(x) / chunk_size))
-  if (count_loci(x) %% chunk_size != 0){
+  if (count_loci(x) %% chunk_size != 0) {
     chunks_vec <- c(chunks_vec, count_loci(x) %% chunk_size)
   }
-  chunks_vec_index <- c(0,cumsum(chunks_vec))
+  chunks_vec_index <- c(0, cumsum(chunks_vec))
 
 
   # generate the header
-  vcf_header <- c("##fileformat=VCFv4.3",
-                  paste0("##fileDate=",format(Sys.time(), "%Y%m%e")),
-                  paste0("##source=tidypopgen_v",utils::packageVersion("tidypopgen")))
+  vcf_header <- c(
+    "##fileformat=VCFv4.3",
+    paste0("##fileDate=", format(Sys.time(), "%Y%m%e")),
+    paste0("##source=tidypopgen_v", utils::packageVersion("tidypopgen"))
+  )
   # create copy of loci table
   loci_tbl <- show_loci(x)
   # reorder chromosomes levels in the order in which they appear
   loci_tbl$chromosome <- forcats::fct_inorder(loci_tbl$chromosome)
   # get max position for each chromosome
-  chromosome_summary <- loci_tbl %>% group_by(.data$chromosome) %>% summarise(max = max(.data$position))
-  for (chrom_i in 1:nrow(chromosome_summary)){
-    vcf_header <- c(vcf_header,
-                    paste0("##contig=<ID=", chromosome_summary$chromosome[chrom_i],
-                           ",length=",chromosome_summary$max[chrom_i]+1,
-                           ">"))
+  chromosome_summary <- loci_tbl %>%
+    group_by(.data$chromosome) %>%
+    summarise(max = max(.data$position))
+  for (chrom_i in seq_len(nrow(chromosome_summary))) {
+    vcf_header <- c(
+      vcf_header,
+      paste0(
+        "##contig=<ID=", chromosome_summary$chromosome[chrom_i],
+        ",length=", chromosome_summary$max[chrom_i] + 1,
+        ">"
+      )
+    )
   }
-  vcf_header <- c(vcf_header, '##INFO=<ID=PR,Number=0,Type=Flag,Description="Provisional reference allele, may not be based on real reference genome">')
-  vcf_header <- c(vcf_header, '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">')
-  vcf_header <- c(vcf_header, paste0("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t",
-                                     paste(x$id, collapse="\t")))
+  vcf_header <- c(vcf_header, '##INFO=<ID=PR,Number=0,Type=Flag,Description="Provisional reference allele, may not be based on real reference genome">') #nolint
+  vcf_header <- c(vcf_header, '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">') #nolint
+  vcf_header <- c(vcf_header, paste0(
+    "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t",
+    paste(x$id, collapse = "\t")
+  ))
   # write it to file
-  utils::write.table(vcf_header, file = file, quote = FALSE, row.names = FALSE,
-              col.names = F)
+  utils::write.table(vcf_header,
+    file = file, quote = FALSE, row.names = FALSE,
+    col.names = FALSE
+  )
   # iterate over genotypes in chunks and append to the vcf body
   for (chunk_i in seq_along(chunks_vec)) {
     genotypes_matrix <- t(show_genotypes(x,
-                                         loci_indices =
-                                           (chunks_vec_index[chunk_i]+1):chunks_vec_index[chunk_i+1]))
-    genotypes_matrix[genotypes_matrix==0] <- "0/0"
-    genotypes_matrix[genotypes_matrix==1] <- "0/1"
-    genotypes_matrix[genotypes_matrix==2] <- "1/1"
+      loci_indices =
+        (chunks_vec_index[chunk_i] + 1):chunks_vec_index[chunk_i + 1]
+    ))
+    genotypes_matrix[genotypes_matrix == 0] <- "0/0"
+    genotypes_matrix[genotypes_matrix == 1] <- "0/1"
+    genotypes_matrix[genotypes_matrix == 2] <- "1/1"
     genotypes_matrix[is.na(genotypes_matrix)] <- "./."
     # subset loci to this chunk
-    loci_sub <- show_loci(x)[(chunks_vec_index[chunk_i]+1):chunks_vec_index[chunk_i+1],]
+    loci_sub <- show_loci(x)[(chunks_vec_index[chunk_i] + 1):chunks_vec_index[chunk_i + 1], ] #nolint
     # add the other columns needed for the
     loci_cols <- c("chromosome", "position", "name", "allele_ref", "allele_alt")
-    loci_sub <- loci_sub %>% select(any_of(loci_cols)) %>%
-      mutate(qual=".",filter=".", info="PR", format = "GT")
+    loci_sub <- loci_sub %>%
+      select(any_of(loci_cols)) %>%
+      mutate(qual = ".", filter = ".", info = "PR", format = "GT")
     loci_sub[is.na(loci_sub)] <- "."
     genotypes_matrix <- cbind(loci_sub, genotypes_matrix)
     # append table to previous chunk
-    utils::write.table(genotypes_matrix, file = file, quote = FALSE, append = TRUE, sep="\t",
-                col.names = FALSE, row.names = FALSE)
+    utils::write.table(genotypes_matrix,
+      file = file, quote = FALSE, append = TRUE, sep = "\t",
+      col.names = FALSE, row.names = FALSE
+    )
   }
   return(file)
-
 }
 
 # TODO for tests
 # read pop_a_vcf
 # rewrite it as a vcf
 # re-read it and check that we got back what we started with
-
