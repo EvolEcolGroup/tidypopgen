95433e26f758be0cc82567f68b3d2abcd5ec32b2
eviecarter33 <eviecarter41@outlook.com>
2025-03-05 11:59:02 +0000

    style and lint

--- R/gt_roh_window.R
+++ R/gt_roh_window.R
@@ -1,99 +1,120 @@
 #' Detect runs of homozygosity using a sliding-window approach
 #'
-#' This function uses a sliding-window approach to look for runs of homozygosity (or
-#' heterozygosity) in a diploid genome. This function uses the package `selectRUNS`,
-#' which implements an approach equivalent to the one in PLINK.
+#' This function uses a sliding-window approach to look for runs of homozygosity
+#' (or heterozygosity) in a diploid genome. This function uses the package
+#' `selectRUNS`, which implements an approach equivalent to the one in PLINK.
 #'
 #' @param x a [gen_tibble]
-#' @param window_size the size of sliding window (number of SNP loci) (default = 15)
+#' @param window_size the size of sliding window (number of SNP loci) (default =
+#'   15)
 #' @param threshold the threshold of overlapping windows of the same state
-#' (homozygous/heterozygous) to call a SNP in a RUN (default = 0.05)
+#'   (homozygous/heterozygous) to call a SNP in a RUN (default = 0.05)
 #' @param min_snp minimum n. of SNP in a RUN (default = 3)
-#' @param heterozygosity should we look for runs of heterozygosity (instead of homozygosity? (default = FALSE)
+#' @param heterozygosity should we look for runs of heterozygosity (instead of
+#'   homozygosity? (default = FALSE)
 #' @param max_opp_window max n. of SNPs of the opposite type (e.g. heterozygous
-#' snps for runs of homozygosity) in the
-#' sliding window (default = 1)
-#' @param max_miss_window max. n. of missing SNP in the sliding window (default = 1)
+#'   snps for runs of homozygosity) in the sliding window (default = 1)
+#' @param max_miss_window max. n. of missing SNP in the sliding window (default
+#'   = 1)
 #' @param max_gap max distance between consecutive SNP to be still considered a
-#' potential run (default = 10^6 bps)
-#' @param min_length_bps minimum length of run in bps (defaults to 1000 bps = 1 kbps)
-#' @param min_density minimum n. of SNP per kbps (defaults to 0.1 = 1 SNP every 10 kbps)
+#'   potential run (default = 10^6 bps)
+#' @param min_length_bps minimum length of run in bps (defaults to 1000 bps = 1
+#'   kbps)
+#' @param min_density minimum n. of SNP per kbps (defaults to 0.1 = 1 SNP every
+#'   10 kbps)
 #' @param max_opp_run max n. of opposite genotype SNPs in the run (optional)
 #' @param max_miss_run max n. of missing SNPs in the run (optional)
 #'
-#' @details
-#' This function returns a data frame with all runs detected in the dataset.
-#' This data frame can then be written out to a csv file.
-#' The data frame is, in turn, the input for other functions of the detectRUNS package
-#' that create plots and produce statistics from the results
-#' (see plots and statistics functions in this manual,
-#' and/or refer to the detectRUNS vignette).
+#' @details This function returns a data frame with all runs detected in the
+#' dataset. This data frame can then be written out to a csv file. The data
+#' frame is, in turn, the input for other functions of the detectRUNS package
+#' that create plots and produce statistics from the results (see plots and
+#' statistics functions in this manual, and/or refer to the detectRUNS
+#' vignette).
 #'
-#' If the [`gen_tibble`] is grouped, then the grouping variable is used to fill in the
-#' group table. Otherwise, the group 'column' is filled with the same values as the 'id'
-#' column
+#' If the [`gen_tibble`] is grouped, then the grouping variable is used to fill
+#' in the group table. Otherwise, the group 'column' is filled with the same
+#' values as the 'id' column
 #'
-#' @return A dataframe with RUNs of Homozygosity or Heterozygosity in the analysed dataset.
-#' The returned dataframe contains the following seven columns: "group", "id", "chrom",
-#' "nSNP", "from", "to", "lengthBps" (group: population, breed, case/control etc.;
-#' id: individual identifier; chrom: chromosome on which the run is located;
-#' nSNP: number of SNPs in the run; from: starting position of the run, in bps;
-#' to: end position of the run, in bps; lengthBps: size of the run)
+#' @return A dataframe with RUNs of Homozygosity or Heterozygosity in the
+#'   analysed dataset. The returned dataframe contains the following seven
+#'   columns: "group", "id", "chrom", "nSNP", "from", "to", "lengthBps" (group:
+#'   population, breed, case/control etc.; id: individual identifier; chrom:
+#'   chromosome on which the run is located; nSNP: number of SNPs in the run;
+#'   from: starting position of the run, in bps; to: end position of the run, in
+#'   bps; lengthBps: size of the run)
 #' @export
 #'
 #' @examples
 #' # run the example only if we have the package installed
 #' if (requireNamespace("detectRUNS", quietly = TRUE)) {
-#' sheep_ped <- system.file("extdata", "Kijas2016_Sheep_subset.ped",
-#'     package="detectRUNS")
-#' sheep_gt <- tidypopgen::gen_tibble(sheep_ped, backingfile = tempfile(),
-#'     quiet=TRUE)
-#' sheep_gt <- sheep_gt %>% group_by(population)
-#' sheep_roh <- gt_roh_window(sheep_gt)
-#' detectRUNS::plot_Runs(runs = sheep_roh)
+#'   sheep_ped <- system.file("extdata", "Kijas2016_Sheep_subset.ped",
+#'     package = "detectRUNS"
+#'   )
+#'   sheep_gt <- tidypopgen::gen_tibble(sheep_ped,
+#'     backingfile = tempfile(),
+#'     quiet = TRUE
+#'   )
+#'   sheep_gt <- sheep_gt %>% group_by(population)
+#'   sheep_roh <- gt_roh_window(sheep_gt)
+#'   detectRUNS::plot_Runs(runs = sheep_roh)
 #' }
-
-
-gt_roh_window <-  function(x, window_size = 15, threshold = 0.05,
-                    min_snp = 3, heterozygosity = FALSE, max_opp_window = 1, max_miss_window = 1,
-                    max_gap = 10^6, min_length_bps = 1000, min_density = 1/1000,
-                    max_opp_run = NULL, max_miss_run = NULL) {
+gt_roh_window <- function(x, window_size = 15, threshold = 0.05,
+                          min_snp = 3, heterozygosity = FALSE,
+                          max_opp_window = 1, max_miss_window = 1,
+                          max_gap = 10^6, min_length_bps = 1000,
+                          min_density = 1 / 1000,
+                          max_opp_run = NULL, max_miss_run = NULL) {
   if (!requireNamespace("detectRUNS", quietly = TRUE)) {
     stop(
       "to use this function, first install package 'detectRUNS' with\n",
-      "install.packages('detectRUNS')")
+      "install.packages('detectRUNS')"
+    )
   }
   # collect all parameters in a variable
-  parameters <- list(windowSize=window_size, threshold=threshold, minSNP=min_snp,
-                     ROHet=heterozygosity, maxOppWindow=max_opp_window,
-                     maxMissWindow=max_miss_window, maxGap=max_gap, minLengthBps=min_length_bps,
-                     minDensity=min_density, maxOppRun=max_opp_run, maxMissRun=max_miss_run)
+  parameters <- list(
+    windowSize = window_size, threshold = threshold, minSNP = min_snp,
+    ROHet = heterozygosity, maxOppWindow = max_opp_window,
+    maxMissWindow = max_miss_window, maxGap = max_gap,
+    minLengthBps = min_length_bps, minDensity = min_density,
+    maxOppRun = max_opp_run, maxMissRun = max_miss_run
+  )
 
   # create a map object
-  map <- show_loci(x) %>% dplyr::select(dplyr::all_of(c("chromosome","name","position"))) %>%
-    dplyr::rename("Chrom"="chromosome", "SNP"="name", "bps"="position") %>% as.data.frame()
+  map <- show_loci(x) %>%
+    dplyr::select(dplyr::all_of(c("chromosome", "name", "position"))) %>%
+    dplyr::rename(
+      "Chrom" = "chromosome",
+      "SNP" = "name", "bps" = "position"
+    ) %>%
+    as.data.frame()
 
   # compute the gaps between snps
   gaps <- diff(map$bps)
   # use groups (if defined)
-  if (dplyr::is_grouped_df(x)){
-    groups <- x %>% select(dplyr::group_vars(x)) %>% dplyr::pull(1)
+  if (dplyr::is_grouped_df(x)) {
+    groups <- x %>%
+      select(dplyr::group_vars(x)) %>%
+      dplyr::pull(1)
   } else {
     groups <- x$id
   }
   # initialize data.frame of results
-  runs_df <- data.frame(group=character(), id=character(), chrom=character(), nSNP=integer(),
-                     from=integer(), to=integer(), lengthBps=integer())
+  runs_df <- data.frame(
+    group = character(), id = character(), chrom = character(),
+    nSNP = integer(), from = integer(), to = integer(), lengthBps = integer()
+  )
   # naively process it by row (the parallelism is implemented within individual)
   # access time is horrible, but I don't think this is the bottleneck
   # it needs some profiling
-  X <- .gt_get_bigsnp(x)$genotypes # pointer for the FBM
-  col_ind <-.gt_bigsnp_cols(x) #column indeces for the snps to consider
-  for (i in seq_len(nrow(x))){
+  X <- .gt_get_bigsnp(x)$genotypes # pointer for the FBM #nolint
+  col_ind <- .gt_bigsnp_cols(x) # column indeces for the snps to consider
+  for (i in seq_len(nrow(x))) {
     this_genotype <- X[i, col_ind]
-    this_indiv <- list(FID=groups[i], IID=x$id[i])
+    this_indiv <- list(FID = groups[i], IID = x$id[i])
     # find runs for this individual
-    this_runs <- utils::getFromNamespace("slidingRuns", "detectRUNS")(this_genotype, this_indiv, map, gaps, parameters)
+    this_runs <-
+      utils::getFromNamespace("slidingRuns", "detectRUNS")(this_genotype, this_indiv, map, gaps, parameters) #nolint
     # bind this run (if has rows) to others RUNs (if any)
     runs_df <- rbind(runs_df, this_runs)
   }
@@ -103,5 +124,4 @@ gt_roh_window <-  function(x, window_size = 15, threshold = 0.05,
 
   # return calculated runs (data.frame)
   return(runs_df)
-
 }
--- R/gt_save.R
+++ R/gt_save.R
@@ -1,99 +1,104 @@
 #' Save a gen_tibble
 #'
 #' Save the tibble (and update the backing files). The `gen_tibble` object is
-#' saved to a file with extension *.gt*, togethe with update its *.rds* and *.bk*
-#' files. Note that multiple *.gt* files can be linked to the same *.rds* and *.bk*
-#' files; generally, this occurs when we create multiple subsets of the data.
-#' The *.gt* file then stores the information on what subset of the full dataset
-#' we are interested in, whilst the *.rds* and *.bk* file store the full dataset.
-#' To reload a `gen_tibble`, you can pass the name of the *.gt* file with
-#' [gt_load()].
+#' saved to a file with extension *.gt*, togethe with update its *.rds* and
+#' *.bk* files. Note that multiple *.gt* files can be linked to the same *.rds*
+#' and *.bk* files; generally, this occurs when we create multiple subsets of
+#' the data. The *.gt* file then stores the information on what subset of the
+#' full dataset we are interested in, whilst the *.rds* and *.bk* file store the
+#' full dataset. To reload a `gen_tibble`, you can pass the name of the *.gt*
+#' file with [gt_load()].
 #'
 #' @param x a [`gen_tibble`]
 #' @param file_name the file name, including the full path. If it does not end
-#' with *.gt*, the extension will be added.
+#'   with *.gt*, the extension will be added.
 #' @param quiet boolean to suppress information about the files
 #' @returns the file name and path of the *.gt* file, together with the *.rds*
-#' and *.bk* files
+#'   and *.bk* files
 #' @seealso [gt_load()]
 #' @export
 
 gt_save <- function(x, file_name = NULL, quiet = FALSE) {
-  if (!inherits(x, "gen_tbl")){
+  if (!inherits(x, "gen_tbl")) {
     stop("x should be a gen_tibble")
   }
   # we update the bigsnp object
-  bigsnpr::snp_save(attr(x$genotypes,"bigsnp"))
+  bigsnpr::snp_save(attr(x$genotypes, "bigsnp"))
 
-  if (is.null(file_name)){
-    file_name <-  bigstatsr::sub_bk(gt_get_file_names(x)[2],".gt")
+  if (is.null(file_name)) {
+    file_name <- bigstatsr::sub_bk(gt_get_file_names(x)[2], ".gt")
   }
-  if (file_ext(file_name)!="gt"){
-    file_name <- paste0(file_name,".gt")
+  if (file_ext(file_name) != "gt") {
+    file_name <- paste0(file_name, ".gt")
   }
   # and now save our gen_tibble
-  saveRDS(x,file_name)
-  if (!quiet){
+  saveRDS(x, file_name)
+  if (!quiet) {
     message("\ngen_tibble saved to ", file_name)
     message("using bigSNP file: ", gt_get_file_names(x)[1])
     message("with backing file: ", gt_get_file_names(x)[2])
     message("make sure that you do NOT delete those files!")
     message("to reload the gen_tibble in another session, use:")
-    message("gt_load('",file_name,"')")
+    message("gt_load('", file_name, "')")
   }
-  return(c(file_name,gt_get_file_names(x)))
+  return(c(file_name, gt_get_file_names(x)))
 }
 
-#' a light version of gt_save that does not resave the bigSNP RDS or backing file, to be used internally
-#' when creating a gen_tibble (if we have just created it, there is not need
-#' to resave it)
+#' a light version of gt_save that does not resave the bigSNP RDS or backing
+#' file, to be used internally when creating a gen_tibble (if we have just
+#' created it, there is not need to resave it)
 #' @param x a [`gen_tibble`]
 #' @param file_name the file name, including the full path. If it does not end
-#' with *.gt*, the extension will be added.
+#'   with *.gt*, the extension will be added.
 #' @param quiet boolean to suppress information about hte files
 #' @returns the file name and path of the *.gt* file, together with the *.rds*
-#' and *.bk* files
+#'   and *.bk* files
 #' @keywords internal
 gt_save_light <- function(x, file_name = NULL, quiet = FALSE) {
-  if (is.null(file_name)){
-    file_name <-  bigstatsr::sub_bk(gt_get_file_names(x)[2],".gt")
+  if (is.null(file_name)) {
+    file_name <- bigstatsr::sub_bk(gt_get_file_names(x)[2], ".gt")
   }
-  if (file_ext(file_name)!="gt"){
-    file_name <- paste0(file_name,".gt")
+  if (file_ext(file_name) != "gt") {
+    file_name <- paste0(file_name, ".gt")
   }
   # and now save our gen_tibble
-  saveRDS(x,file_name)
-  if (!quiet){
+  saveRDS(x, file_name)
+  if (!quiet) {
     message("\ngen_tibble saved to ", file_name)
     message("using bigSNP file: ", gt_get_file_names(x)[1])
     message("with backing file: ", gt_get_file_names(x)[2])
     message("make sure that you do NOT delete those files!")
     message("to reload the gen_tibble in another session, use:")
-    message("gt_load('",file_name,"')")
+    message("gt_load('", file_name, "')")
   }
-  return(c(file_name,gt_get_file_names(x)))
+  return(c(file_name, gt_get_file_names(x)))
 }
 
 
-sub_gt <- function (path, replacement = "", stop_if_not_ext = TRUE)
-{
+sub_gt <- function(path, replacement = "", stop_if_not_ext = TRUE) {
   pattern <- "\\.gt$"
-  if (!grepl(pattern, path))
+  if (!grepl(pattern, path)) {
     stop("Path '%s' must have 'gt' extension.", path)
-  if (stop_if_not_ext && (nchar(replacement) > 0) && (substr(replacement,
-                                                             1, 1) != "."))
+  }
+  if (stop_if_not_ext && (nchar(replacement) > 0) && (substr(
+    replacement,
+    1, 1
+  ) != ".")) {
     stop("Replacement must be an extension starting with '.' if provided.")
+  }
   sub(pattern, replacement, path)
 }
 
 #' Get the names of files storing the genotypes of a `gen_tibble`
 #'
-#' A function to return the names of the files used to store data in a `gen_tibble`.
-#' Specifically, this returns the .rds file storing the big
+#' A function to return the names of the files used to store data in a
+#' `gen_tibble`. Specifically, this returns the .rds file storing the big
 #' @param x a [`gen_tibble`]
 #' @returns a character vector with the names and paths of the two files
 #' @export
-gt_get_file_names <- function(x){
-  return(c(attr(x$genotypes,"bigsnp")$genotypes$rds,
-    attr(x$genotypes,"bigsnp")$genotypes$backingfile))
+gt_get_file_names <- function(x) {
+  return(c( # nolint
+    attr(x$genotypes, "bigsnp")$genotypes$rds,
+    attr(x$genotypes, "bigsnp")$genotypes$backingfile
+  ))
 }
