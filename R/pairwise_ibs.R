d324fda00a64a2e10d316e1c30b5559adad96840
eviecarter33 <eviecarter41@outlook.com>
2025-03-05 16:32:43 +0000

    styling and linting pairwise functions

--- R/pairwise_ibs.R
+++ R/pairwise_ibs.R
@@ -2,41 +2,47 @@
 #'
 #' This function computes the IBS matrix.
 #'
-#' Note that monomorphic sites are currently considered. Remove monomorphic sites
-#' before running pairwise_king if this is a concern.
+#' Note that monomorphic sites are currently considered. Remove monomorphic
+#' sites before running pairwise_king if this is a concern.
 #' @param x a `gen_tibble` object.
-#' @param as_matrix boolean, determining whether the results should be a square symmetrical matrix (TRUE),
-#' or a tidied tibble (FALSE, the default)
-#' @param type one of "proportion" (equivalent to "ibs" in PLINK), "adjusted_counts" ("distance" in PLINK),
-#' and "raw_counts" (the counts of identical alleles and non-missing alleles, from which the two other quantities are computed)
-#' @param block_size maximum number of loci read at once. More loci should improve speed,
-#' but will tax memory.
+#' @param as_matrix boolean, determining whether the results should be a square
+#'   symmetrical matrix (TRUE), or a tidied tibble (FALSE, the default)
+#' @param type one of "proportion" (equivalent to "ibs" in PLINK),
+#'   "adjusted_counts" ("distance" in PLINK), and "raw_counts" (the counts of
+#'   identical alleles and non-missing alleles, from which the two other
+#'   quantities are computed)
+#' @param block_size maximum number of loci read at once. More loci should
+#'   improve speed, but will tax memory.
 #' @returns a [bigstatsr::FBM] of proportion or adjusted counts, or a list of
-#' two [bigstatsr::FBM] matrices, one of counts of IBS by alleles,
-#' and one of number of valid alleles (i.e. 2*n_loci - 2*missing_loci)
+#'   two [bigstatsr::FBM] matrices, one of counts of IBS by alleles, and one of
+#'   number of valid alleles (i.e. 2*n_loci - 2*missing_loci)
 #' @export
 pairwise_ibs <- function(x, as_matrix = FALSE,
-                      type = c("proportion","adjusted_counts","raw_counts"),
-                      block_size = bigstatsr::block_size(count_loci(x))) {
+                         type = c(
+                           "proportion",
+                           "adjusted_counts",
+                           "raw_counts"
+                         ),
+                         block_size = bigstatsr::block_size(count_loci(x))) {
   type <- match.arg(type)
-  X <- attr(x$genotypes,"bigsnp") # convenient pointer
+  X <- attr(x$genotypes, "bigsnp") # convenient pointer #nolint
   x_ind_col <- show_loci(x)$big_index
   x_ind_row <- vctrs::vec_data(x$genotypes)
   ibs_matrix <- snp_ibs(X$genotypes,
-            ind.row = x_ind_row,
-            ind.col = x_ind_col,
-            type = type,
-            block.size = block_size)
-  #dimnames(ibs_matrix)<-list(x$id, x$id)
-  if (inherits(ibs_matrix,"matrix")){
-    dimnames(ibs_matrix)<-list(x$id, x$id)
+    ind.row = x_ind_row,
+    ind.col = x_ind_col,
+    type = type,
+    block.size = block_size
+  )
+  if (inherits(ibs_matrix, "matrix")) {
+    dimnames(ibs_matrix) <- list(x$id, x$id)
   } else { # else if we have a list of two count matrices
-    attr(ibs_matrix[[1]],"indiv_names") <- x$id
-    attr(ibs_matrix[[2]],"indiv_names") <- x$id
+    attr(ibs_matrix[[1]], "indiv_names") <- x$id
+    attr(ibs_matrix[[2]], "indiv_names") <- x$id
   }
 
-  if(type == "proportion"|type == "adjusted_counts"){
-    if (as_matrix){
+  if (type == "proportion" || type == "adjusted_counts") {
+    if (as_matrix) {
       return(ibs_matrix)
     } else {
       return(tidy_dist_matrix(ibs_matrix))
--- R/pairwise_king.R
+++ R/pairwise_king.R
@@ -2,29 +2,28 @@
 #'
 #' This function computes the KING-robust estimator of kinship.
 #'
-#' Note that monomorphic sites are currently considered. Remove monomorphic sites
-#' before running pairwise_king if this is a concern.
+#' Note that monomorphic sites are currently considered. Remove monomorphic
+#' sites before running pairwise_king if this is a concern.
 #' @param x a `gen_tibble` object.
-#' @param as_matrix boolean, determining whether the results should be a square symmetrical matrix (TRUE),
-#' or a tidied tibble (FALSE, the default)
-#' @param block_size maximum number of loci read at once. More loci should improve speed,
-#' but will tax memory.
+#' @param as_matrix boolean, determining whether the results should be a square
+#'   symmetrical matrix (TRUE), or a tidied tibble (FALSE, the default)
+#' @param block_size maximum number of loci read at once. More loci should
+#'   improve speed, but will tax memory.
 #' @export
 pairwise_king <- function(x, as_matrix = FALSE,
-                      block_size = bigstatsr::block_size(length(loci_names(x)))) {
-
-  X <- attr(x$genotypes,"bigsnp") # convenient pointer
+                          block_size = bigstatsr::block_size(length(loci_names(x)))) { #nolint
+  X <- attr(x$genotypes, "bigsnp") # convenient pointer #nolint
   x_ind_col <- show_loci(x)$big_index
   x_ind_row <- vctrs::vec_data(x$genotypes)
   king_matrix <- snp_king(X$genotypes,
-            ind.row = x_ind_row,
-            ind.col = x_ind_col,
-            block.size = block_size)
-  dimnames(king_matrix)<-list(x$id, x$id)
-  if (as_matrix){
+    ind.row = x_ind_row,
+    ind.col = x_ind_col,
+    block.size = block_size
+  )
+  dimnames(king_matrix) <- list(x$id, x$id)
+  if (as_matrix) {
     return(king_matrix)
   } else {
     return(tidy_dist_matrix(king_matrix))
   }
-
 }
