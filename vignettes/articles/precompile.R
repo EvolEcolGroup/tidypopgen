48d1cba6b3ae951c966337057939d55c11c0b524
eviecarter33 <eviecarter41@outlook.com>
2025-04-01 11:18:32 +0100

    Reorder benchmarking summary and add total time

--- vignettes/articles/precompile.R
+++ vignettes/articles/precompile.R
@@ -1,4 +1,6 @@
 # to be sourced from within the vignette/articles directory
-setwd(getSrcDirectory(function(x) {x}))
+setwd(getSrcDirectory(function(x) {
+  x
+}))
 unlink("./figure")
 knitr::knit("benchmark_hgdp.Rmd.orig", "benchmark_hgdp.Rmd")
