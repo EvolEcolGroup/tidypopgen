e157f50fc93e3d5376610ec9ec439284391bd637
Andrea Manica <am315@cam.ac.uk>
2025-04-01 09:56:25 +0100

    update article

--- vignettes/articles/precompile.R
+++ vignettes/articles/precompile.R
@@ -1,3 +1,4 @@
 # to be sourced from within the vignette/articles directory
-
+setwd(getSrcDirectory(function(x) {x}))
+unlink("./figure")
 knitr::knit("benchmark_hgdp.Rmd.orig", "benchmark_hgdp.Rmd")
