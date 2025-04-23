8d945f950efb1f5202d5dd1cea0a0ed809a82fbc
Andrea Manica <am315@cam.ac.uk>
2025-04-23 08:52:01 +0100

    fix lints

--- R/cbind_gen_tibble.R
+++ R/cbind_gen_tibble.R
@@ -1,14 +1,14 @@
 #' Combine a gen_tibble to a data.frame or tibble by column
 #'
-#' A [cbind()] method to merge `gen_tibble` objects with data.frames and
-#' normal tibbles. Whilst this works, it is not
-#' ideal as it does not check the order of the tables, and we suggest that you use
-#' [`dplyr::left_join()`] instead. Note that `cbind` will not combine two
-#' `gen_tibbles` (i.e. it will NOT combine markers for the same individuals)
+#' A [cbind()] method to merge `gen_tibble` objects with data.frames and normal
+#' tibbles. Whilst this works, it is not ideal as it does not check the order of
+#' the tables, and we suggest that you use [`dplyr::left_join()`] instead. Note
+#' that `cbind` will not combine two `gen_tibbles` (i.e. it will NOT combine
+#' markers for the same individuals)
 #'
 #' @param ... a gen_tibble and a data.frame or tibble
 #' @param deparse.level an integer controlling the construction of column names.
-#' See \code{\link[base]{cbind}} for details.
+#'   See \code{\link[base]{cbind}} for details.
 #' @returns a `gen_tibble`
 #' @export
 
