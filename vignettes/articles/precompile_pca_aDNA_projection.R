48daaf76861ea6dc3178251490ceb89d7fd0728d
eviecarter33 <eviecarter41@outlook.com>
2025-04-27 12:56:28 +0100

    change download to eval = FALSE

--- vignettes/articles/precompile_pca_aDNA_projection.R
+++ vignettes/articles/precompile_pca_aDNA_projection.R
@@ -3,6 +3,4 @@ setwd(getSrcDirectory(function(x) {
   x
 }))
 unlink("./figure/pca_aDNA_projection")
-unlink("./data/NearEastPublic", recursive = "TRUE")
-unlink("./data/NearEastPublic.tar.gz")
 knitr::knit("pca_aDNA_projection.Rmd.orig", "pca_aDNA_projection.Rmd")
