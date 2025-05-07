f0111c02e3c6457154910acfdd4f0325743123ac
Andrea Manica <am315@cam.ac.uk>
2025-05-07 22:34:51 +0100

    read the correct bits

--- src/read_packedancestry.cpp
+++ src/read_packedancestry.cpp
@@ -11,6 +11,12 @@ using namespace std;
 
 #define PACK_DENSITY 4
 
+/* 3 is 11 in binary, we need a 2 bit mask for each of the 4 positions */
+#define MASK0 3	  /* 3 << 2 * 0 */
+#define MASK1 12  /* 3 << 2 * 1 */
+#define MASK2 48  /* 3 << 2 * 2 */
+#define MASK3 192 /* 3 << 2 * 3 */
+
 // [[Rcpp::export]]
 bool read_packedancestry(const char * filename,
               Environment BM,
@@ -22,9 +28,9 @@ bool read_packedancestry(const char * filename,
   int n = xpBM->nrow(); // number of individuals
   int m = xpBM->ncol(); // number of loci
   
-  int length = n / 4;
-  int extra = n - 4 * length;
-  int lengthExtra = length + (extra > 0);
+  int length = n / 4; // number of complete packed bytes
+  int extra = n - 4 * length; //extra indivs in the last byte
+  int lengthExtra = length + (extra > 0); // total number of bites with information
   int j, k;
   
   unsigned char *buffer = new unsigned char[std::max(3, lengthExtra) + 1];
@@ -35,30 +41,62 @@ bool read_packedancestry(const char * filename,
   myFile.seekg(0, std::ifstream::end);
   // file size in bytes
   long long len = (long long)myFile.tellg();
-  Rcout<<"len is "<<len << endl;
+  // byets per snp (there are additional empty bytes at the end of lines)
   long long bytespersnp = len/(m+1);
-  Rcout<<" bytespersnp is "<<bytespersnp<<endl;
-  
-  
 
+  // skip the first row (which is metadata on n_ind and n_snps)
+  myFile.seekg((0+1)*bytespersnp, std::ifstream::beg);
+  
+  // buffer to read in
+  unsigned char* tmp = new unsigned char[bytespersnp];
+  char tmpi;
+  
   for (j = 0; j < m; j++) {
     // read from bedfile
-    myFile.read((char*)buffer, lengthExtra);
+    //myFile.read((char*)buffer, lengthExtra);
+    Rcout<<"locus "<< j << endl;
+    // read raw genotypes
+    myFile.read((char*)tmp, sizeof(char) * bytespersnp);
     
     for (k = 0; k < length; k++) {
-      code_ptr = &tab(0, buffer[k]);
+      code_ptr = &tab(0, tmp[k]);
       ptr = std::copy(code_ptr, code_ptr + 4, ptr);
+      // Rcout<< "locus "<< j << "\t" << k << "\t" << buffer[k] << endl;
+      // Rcout<< "locus "<< j << "\t" << k << "\t" << (int)buffer[k] << endl;
+      
+      tmpi = tmp[k];
+      Rcout<< "\t" << ((tmpi & MASK3) >> 6);
+      Rcout<< "\t" << ((tmpi & MASK2) >> 4);
+      Rcout<< "\t" << ((tmpi & MASK1) >> 2);
+      Rcout<< "\t" << (tmpi & MASK0);
     }
-    if (extra) {
-      code_ptr = &tab(0, buffer[k]);
+    if (extra){
+      code_ptr = &tab(0, tmp[k]);
       ptr = std::copy(code_ptr, code_ptr + extra, ptr);
+      
+      tmpi = tmp[k];
+      Rcout<< "\t" << ((tmpi & MASK3) >> 6);
+      Rcout<< "\t" << ((tmpi & MASK2) >> 4);
+      Rcout<< "\t" << ((tmpi & MASK1) >> 2);
+      Rcout<< "\t" << (tmpi & MASK0);
     }
+    Rcout<<endl;
   }
+   
+    // for (k = 0; k < length; k++) {
+    //   code_ptr = &tab(0, buffer[k]);
+    //   ptr = std::copy(code_ptr, code_ptr + 4, ptr);
+    // }
+    // if (extra) {
+    //   code_ptr = &tab(0, buffer[k]);
+    //   ptr = std::copy(code_ptr, code_ptr + extra, ptr);
+    // }
+
   
   char c;
-  bool is_eof = !(myFile.get(c));
+//  bool is_eof = !(myFile.get(c));
   myFile.close();
   delete[] buffer;
-  
-  return is_eof;
+  delete[] tmp;
+  return true;
 }
