90988ec4c45644d4287d8c27c9af767d99a028d9
Andrea Manica <am315@cam.ac.uk>
2025-05-20 22:59:09 +0100

    draft infrastructure for pseudohaploids

---
+++ R/as_pseudohaploid.R
@@ -0,0 +1,48 @@
+#' Set the ploidy of a `gen_tibble` to include pseudohaploids
+#' 
+#' The ploidy of a `gen_tibble` is set to -2, to indicate that some individuals
+#' are coded as pseudohaploids. The ploidy of the individuals is updated, with
+#' pseudohaploids set to 1 and diploids set to 2. However, the dosages are not
+#' changed, meaning that pseudohaploids are still coded as 0 or 2.
+#' 
+#' @param x a `gen_tibble` object
+#' @param test_n_loci the number of loci to test to determine if an individual
+#' is pseudohaploid. If there are no heterozygoes in the first `test_n_loci`
+#' loci, the individual is considered a pseudohaploid. If `NULL`, all loci are
+#' tested.
+#' @return a `gen_tibble` object with the ploidy set to -2 and the individual
+#' ploidy values updated to 1 or 2.
+#' @export
+
+as_pseudohaploid <- function(x, test_n_loci = 10000) {
+  # check that the input is a gen_tibble
+  stopifnot_gen_tibble(x)
+  
+  # if this is already set to pseudohaploid, reset it to diploid to reprocess it
+  if (attr(x$genotypes, "ploidy") == -2) {
+    attr(x$genotypes, "ploidy") <- 2
+  }
+
+  # get the number of loci
+  n_loci <- count_loci(x)
+  
+  # if test_n_loci is NULL, set it to n_loci
+  if (is.null(test_n_loci)) {
+    test_n_loci <- n_loci
+  }
+  # if test_n_loci greater than n_loci, set it to n_loci
+  test_n_loci <- min(test_n_loci, n_loci)
+  
+  if (!"ploidy" %in% names(attr(x, "bigsnp")$fam)){
+    attr(x, "bigsnp")$fam$ploidy <- NA_integer_
+  }
+  
+  attr(x, "bigsnp")$fam$ploidy[.gt_bigsnp_rows(x)] <-
+    identify_pseudohaploids (x, n_test = test_n_loci)
+  
+  # set ploidy to -2
+  attr(x$genotypes, "ploidy") <- -2
+  
+  
+  return(x)
+}
---
+++ tests/testthat/test_as_pseudohaploid.R
@@ -0,0 +1,46 @@
+test_genotypes <- rbind(
+  c(1, 1, 0, 1, 1, 0),
+  c(2, 2, 0, NA, 0, 0),
+  c(2, NA, 0, 0, 1, 1),
+  c(2, 0, 0, 2, 0, 0),
+  c(1, 2, 0, 1, 2, 1),
+  c(0, 0, 0, 0, NA, 2),
+  c(0, 1, 2, 0, 1, NA)
+)
+test_indiv_meta <- data.frame(
+  id = c("a", "b", "c", "d", "e", "f", "g"),
+  population = c("pop1", "pop1", "pop2", "pop2", "pop1", "pop3", "pop3")
+)
+test_loci <- data.frame(
+  name = paste0("rs", 1:6),
+  chromosome = paste0("chr", c(1, 1, 1, 1, 2, 2)),
+  position = as.integer(c(3, 5, 65, 343, 23, 456)),
+  genetic_dist = as.double(rep(0, 6)),
+  allele_ref = c("A", "T", "C", "G", "C", "T"),
+  allele_alt = c("T", "C", NA, "C", "G", "A")
+)
+
+test_gt <- gen_tibble(
+  x = test_genotypes,
+  loci = test_loci,
+  indiv_meta = test_indiv_meta,
+  quiet = TRUE
+)
+test_gt <- test_gt %>% group_by(population)
+
+test_that("as_pseudohaploids correctly deals with ploidy",{
+  # confirm that the gen_tibble is diploid for the moment
+  expect_equal(show_ploidy(test_gt), 2)
+  # now use as_pseudohaploid
+  test_gt_pseudo <- as_pseudohaploid(test_gt)
+  # now check that ploidy is -2
+  expect_equal(show_ploidy(test_gt_pseudo), -2)
+  ## TODO check the individual ploidies
+  
+  ## TODO check that we can reprocess it (use a different number of loci to get different result)
+})
+
+
+## TODO now test which functions work and which fail with pseudohaploid data (we should get the right frequencies (grouped and ungrouped, and pairwise pop fst))
+# missingness shoudl also work
+# but most other indiv stats will fail, and so will pop estimates that requires heterozygote counts
