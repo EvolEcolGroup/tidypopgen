76cfe1980b6622fc01df60d0fdad2e5fb128b29f
eviecarter33 <eviecarter41@outlook.com>
2025-06-09 07:29:52 +0100

    examples for gt_as_ functions

--- R/gt_as_hierfstat.R
+++ R/gt_as_hierfstat.R
@@ -7,8 +7,14 @@
 #' @returns a data.frame with a column 'pop' and further column representing
 #' the genotypes (with alleles recoded as 1 and 2)
 #' @export
+#' @examples
+#' example_gt <- example_gt("gen_tbl")
 #'
-
+#' # Convert to genind
+#' gt_hierfstat <- example_gt %>% gt_as_hierfstat()
+#'
+#' # Check object class
+#' class(gt_hierfstat)
 gt_as_hierfstat <- function(x) {
   hier_df <- data.frame(pop = as.factor(x$population), show_genotypes(x))
   hier_df[hier_df == 0] <- 11
