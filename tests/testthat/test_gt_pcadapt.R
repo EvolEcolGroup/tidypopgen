d59b1bccdaa85c088dc4161d42d85c21888ca6ab
eviecarter33 <eviecarter41@outlook.com>
2026-04-30 16:43:41 +0100

    update test

--- tests/testthat/test_gt_pcadapt.R
+++ tests/testthat/test_gt_pcadapt.R
@@ -34,6 +34,12 @@ test_that("gt_pcadapt on gen_tbl vcf",{
   anolis <- gt_impute_simple(anolis, method = "mode")
   anolis <- anolis %>% select_loci_if(loci_maf(genotypes) > 0)
   anolis <- gt_update_backingfile(anolis)
+
+  dim(.gt_get_fbm(anolis))
+  gt_uses_imputed(anolis)
+  gt_has_imputed(anolis)
+  gt_set_imputed(anolis, TRUE)
+
   pop_b_vcf_pca <- gt_pca_randomSVD(anolis)
   pop_b_vcf_pcadapt <- gt_pcadapt(anolis, pop_b_vcf_pca, k = 2)
 })
